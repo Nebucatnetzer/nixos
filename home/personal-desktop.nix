257d9742afa6b43e7720d0955dc552a2b55cf788
Andreas Zweili <andreas@zweili.ch>
2021-11-16 18:49:24 +0100

    add a git config for personal use

--- home/personal-desktop.nix
+++ home/personal-desktop.nix
@@ -3,10 +3,20 @@
   imports = [
     ./common.nix
   ];
+  home.packages = with pkgs; [
+  ];
   programs.git = {
     enable = true;
     userName = "Andreas Zweili";
     userEmail = "andreas@zweili.ch";
+    delta = {
+      enable = true;
+      options = {
+        navigate = true;
+        line-numbers = true;
+        syntax-theme = "GitHub";
+      };
+    };
   };
   # raw config files
   home.file.".bashrc".source = ./personal_config/bashrc;

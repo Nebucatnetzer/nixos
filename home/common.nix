fe37a326ca68b76776ea8fd1472c6debb3f09831
Andreas Zweili <zweili@contria.com>
2021-11-16 14:19:41 +0100

    try to configure vim

--- home/common.nix
+++ home/common.nix
@@ -22,8 +22,17 @@
   home.packages = with pkgs; [
     tmux
     mosh
-    vim
     ansible
-    git
   ];
+
+  programs.vim = {
+    enable = true;
+    settings = {
+      expandtab = true;
+      tabstop = 4;
+      shiftwidth = 4;
+      number = true;
+      relativenumber = true;
+    };
+  };
 }

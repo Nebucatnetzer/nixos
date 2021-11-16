05fbef46da400d0389b848d1215aeb624e5c9d3a
Andreas Zweili <andreas@zweili.ch>
2021-11-16 20:31:35 +0100

    fix the home-manager version

--- home/common.nix
+++ home/common.nix
@@ -14,7 +14,7 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "21.11";
+  home.stateVersion = "21.05";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;

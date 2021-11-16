6473a5215edbbbe9a31d2f8770f78690e3c21068
Andreas Zweili <zweili@contria.com>
2021-11-16 14:19:30 +0100

    add delta to the work git

--- home/work-wsl.nix
+++ home/work-wsl.nix
@@ -7,6 +7,14 @@
     enable = true;
     userName = "Andreas Zweili";
     userEmail = "zweili@contria.com";
+    delta = {
+      enable = true;
+      options = {
+        navigate = true;
+        line-numbers = true;
+        syntax-theme = "GitHub";
+      };
+    };
   };
   # raw config files
   home.file.".bashrc".source = ./work_config/bashrc;

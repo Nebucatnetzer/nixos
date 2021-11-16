218d12deb75872e86f4a43b05644188ad5c04078
Andreas Zweili <zweili@contria.com>
2021-11-16 15:06:24 +0100

    try to use a shell alias

--- home/work-wsl.nix
+++ home/work-wsl.nix
@@ -16,6 +16,9 @@
       };
     };
   };
+  home.shellAliases = {
+     management-server = "mosh --ssh=\"ssh -p 22\" localadmin@10.40.0.53 tmux a";
+  };
   # raw config files
   home.file.".bashrc".source = ./work_config/bashrc;
 }

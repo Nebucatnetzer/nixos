03d666326e808532274c52fd680aa09a28db949e
Andreas Zweili <andreas@zweili.ch>
2021-11-23 21:27:52 +0100

    remove unused line

--- hardware/xps5530/hardware-configuration.nix
+++ hardware/xps5530/hardware-configuration.nix
@@ -4,10 +4,6 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
-
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "aesni_intel" "cryptd" ];
   boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-intel" ];

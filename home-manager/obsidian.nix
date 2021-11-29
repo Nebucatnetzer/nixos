e583a00feee5c8d57b1f3e6a8400f9772215e613
Andreas Zweili <andreas@zweili.ch>
2021-11-29 16:10:06 +0100

    add obsidian

--- home-manager/obsidian.nix
+++ home-manager/obsidian.nix
@@ -1,14 +1,14 @@
 { config, pkgs, ... }:
 let
   unstable = import
-    (builtins.fetchGit {
+    (builtins.fetchTarball {
       # Descriptive name to make the store path easier to identify
-      url = "https://github.com/nixos/nixpkgs/";
+      url = "https://github.com/nixos/nixpkgs/archive/942eb9a335b4cd22fa6a7be31c494e53e76f5637.tar.gz";
       # Commit hash for nixos-unstable as of 2018-09-12
       # `git ls-remote https://github.com/nixos/nixpkgs nixos-unstable`
-      ref = "refs/heads/nixos-unstable";
-      rev = "942eb9a335b4cd22fa6a7be31c494e53e76f5637";
-    }) # reuse the current configuration
+      sha256 = "05390093gl44h8v6pgklwkgbn3vwdhs81shabqmjagq6rg1sh1l5";
+    })
+    # reuse the current configuration
     {
       config = config.nixpkgs.config;
     };

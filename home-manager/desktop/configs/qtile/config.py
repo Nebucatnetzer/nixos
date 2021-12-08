f78785c91109401766bc99ae804c93d756416903
Andreas Zweili <andreas@zweili.ch>
2021-12-08 13:48:27 +0100

    bind linephone windows to screen 8

--- home-manager/desktop/configs/qtile/config.py
+++ home-manager/desktop/configs/qtile/config.py
@@ -48,7 +48,7 @@ keys = [
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
 
     Key([mod, "shift"], "q", lazy.window.kill()),
-    Key([mod, "control"], "r", lazy.restart(), desc="Restart qtile"),
+    Key([mod, "control"], "r", lazy.reload_config(), desc="Restart qtile"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown qtile"),
 
     Key([mod], "F1", lazy.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle")),
@@ -80,7 +80,9 @@ group_matches = [
         Match(wm_class="JDownloader"),
     ], #6
     None, #7
-    None, #8
+    [
+        Match(wm_class="linphone"),
+    ], #8
     [
         Match(wm_class="Steam"),
     ], #9

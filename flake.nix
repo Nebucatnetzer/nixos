e9bf375f7063d251c06037af93fdc3ca54ed2b3b
Andreas Zweili <andreas@zweili.ch>
2021-12-11 14:44:46 +0100

    add inputs

--- flake.nix
+++ flake.nix
@@ -1,5 +1,19 @@
 {
-  description = "My system configuration";
+  description = "Andreas Zweili's Nixos configuration";
+
+  inputs = {
+    system-version = import ../version.nix;
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-" + system-version;
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-" + system-version;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  flake-utils.url = "github:numtide/flake-utils";
+
 
   outputs = { self, nixpkgs }: { };
 }

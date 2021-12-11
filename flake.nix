2ce1de598f247f3d837fa01f9dae2c79f770b280
Andreas Zweili <andreas@zweili.ch>
2021-12-11 15:21:01 +0100

    fix some things

--- flake.nix
+++ flake.nix
@@ -2,18 +2,19 @@
   description = "Andreas Zweili's Nixos configuration";
 
   inputs = {
-    system-version = import ../version.nix;
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-" + system-version;
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-
+    flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
-      url = "github:nix-community/home-manager/release-" + system-version;
+      url = "github:nix-community/home-manager/release-21.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  flake-utils.url = "github:numtide/flake-utils";
-
-
-  outputs = { self, nixpkgs }: { };
+  outputs = inputs @ { self, nixpkgs, nixpkgs-unstable, flake-utils, home-manager }: {
+    nixosConfigurations.gwyn = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./hardware/precision/configuration.nix ];
+    };
+  };
 }

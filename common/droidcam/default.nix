40cbf04a8e8d0e3b11e836bb9d0500d09376a027
Andreas Zweili <andreas@zweili.ch>
2021-12-22 11:04:31 +0100

    add an alias for droidcam

--- common/droidcam/default.nix
+++ common/droidcam/default.nix
@@ -7,5 +7,8 @@
   programs.droidcam.enable = true;
   # required for USB connection
   services.usbmuxd.enable = true;
+  environment.shellAliases = {
+    webcam = "droidcam-cli -size=1920x1080 ios 4747";
+  };
 }
 

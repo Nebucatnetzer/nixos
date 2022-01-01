ccbd0de798c8dfaf7aaf4f9c619f643f1bf0d1fb
Andreas Zweili <andreas@zweili.ch>
2022-01-01 15:21:51 +0100

    add a config for proxmox-vms

---
+++ systems/proxmox-vm/configuration.nix
@@ -0,0 +1,16 @@
+{ ... }:
+
+{
+  imports =
+    [
+      # Include the results of the hardware scan.
+      ../../modules/desktop.nix
+      ./hardware-configuration.nix
+    ];
+
+  networking = {
+    hostName = "nixos-test-vm";
+    interfaces.enp0s18.useDHCP = true;
+  };
+}
+
---
+++ systems/proxmox-vm/hardware-configuration.nix
@@ -0,0 +1,31 @@
+{ ... }:
+
+{
+  boot.initrd.availableKernelModules = [
+    "ata_piix"
+    "uhci_hcd"
+    "virtio_pci"
+    "virtio_scsi"
+    "sd_mod"
+    "sr_mod"
+  ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "dm-snapshot" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/BOOT";
+    fsType = "vfat";
+  };
+
+  swapDevices = [
+    { device = "/dev/disk/by-label/swap"; }
+  ];
+
+  services.qemuGuest.enable = true;
+}

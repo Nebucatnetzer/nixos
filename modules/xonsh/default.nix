e8cad302b768cf6dfc012e8cb6887395b2cb6217
Andreas Zweili <andreas@zweili.ch>
2022-01-03 15:16:26 +0100

    add alias for nixpkgs-fmt

--- modules/xonsh/default.nix
+++ modules/xonsh/default.nix
@@ -10,6 +10,7 @@ in
       aliases['nix-generations'] = "sudo nix-env --list-generations --profile /nix/var/nix/profiles/system"
       aliases['rebuild'] = "sudo nixos-rebuild -j auto switch"
       aliases['find-garbage'] = "ls -l /nix/var/nix/gcroots/auto/ | sort"
+      aliases['format-module'] = "nixpkgs-fmt **/*.nix"
     '';
   };
   users.users.${username} = {

ebc43329686eefd0f52afc13da415ec98644dd98
Andreas Zweili <andreas@zweili.ch>
2022-01-03 20:00:40 +0100

    add a nix shell
    credits @wiltaylor

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell {
+  name = "nixosbuildshell";
+  nativeBuildInputs = with pkgs; [
+    git
+    nixFlakes
+  ];
+
+  shellHook = ''
+    PATH=${pkgs.writeShellScriptBin "nix" ''
+      ${pkgs.nixFlakes}/bin/nix --experimental-features "nix-command flakes" "$@"
+    ''}/bin:$PATH
+  '';
+}

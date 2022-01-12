a62d95dac56c9f7420eb23dfce261c90aa3d70ef
Andreas Zweili <andreas@zweili.ch>
2022-01-12 21:53:09 +0100

    add a script to setup the VM home

---
+++ scripts/setup_vm_home.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+git clone https://git.2li.ch/Nebucatnetzer/nixos.git /home/andreas/.nixos
+sudo ln -s /home/andreas/.nixos/flake.nix /etc/nixos/
+git clone https://git.2li.ch/Nebucatnetzer/docker_systems

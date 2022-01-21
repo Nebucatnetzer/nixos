d6b22d2d8176e671d4010ed174d2204900ff194c
Andreas Zweili <andreas@zweili.ch>
2022-01-21 20:03:33 +0100

    add mimetype for mpv

--- home-manager/software/mpv/default.nix
+++ home-manager/software/mpv/default.nix
@@ -6,4 +6,11 @@
       s = "playlist-shuffle";
     };
   };
+  xdg.mimeApps = {
+    enable = true;
+    associations.added = {
+      "application/pdf" = ["org.gnome.Evince.desktop"];
+      "inode/directory" = ["mpv.desktop"];
+    };
+  };
 }

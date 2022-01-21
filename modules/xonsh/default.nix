05d57dd7f1e85a89cbd5c21bba67faaee427447b
Andreas Zweili <zweili@contria.com>
2022-01-21 13:48:56 +0100

    set the colour theme for xonsh

--- modules/xonsh/default.nix
+++ modules/xonsh/default.nix
@@ -12,6 +12,7 @@ in
       aliases['find-garbage'] = "ls -l /nix/var/nix/gcroots/auto/ | sort"
       aliases['format-module'] = "nixpkgs-fmt **/*.nix"
       aliases['vm'] = "vim"
+      $XONSH_COLOR_STYLE = 'solarized-light'
     '';
   };
   users.users.${username} = {

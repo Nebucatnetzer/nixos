ee24fba06cb719d362c0927f812bfd8e2d2801bc
Andreas Zweili <andreas@zweili.ch>
2022-01-24 11:04:16 +0100

    remove unused alias

--- home-manager/desktop.nix
+++ home-manager/desktop.nix
@@ -41,9 +41,6 @@
 
   programs.bash = {
     enable = true;
-    shellAliases = {
-      management-server = "mosh --ssh='ssh -p 22' andreas@10.7.89.106 tmux a";
-    };
   };
 
   services.nextcloud-client = {

20b2c874a4df406d4b0a6529a6db50281f9d6445
Andreas Zweili <andreas@zweili.ch>
2022-01-24 12:44:09 +0100

    extend home-manager setup script for non-nixos

--- scripts/install-home-manager.sh
+++ scripts/install-home-manager.sh
@@ -1,4 +1,6 @@
 nix-shell '<home-manager>' -A install
 rm ~/.config/nixpkgs/home.nix
-ln -s $(pwd)/home-manager/$ZWEILI_ENVIRONMENT.nix /home/$USER/.config/nixpkgs/home.nix
+ln -s $(pwd)/.nixos/flake.nix /home/$USER/.config/nixpkgs/flake.nix
+mkdir -p ~/.config/nix
+echo "experimental-features = nix-command flakes" > ~/.config/nix/nix.con
 home-manager switch

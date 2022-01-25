50b0be41982d8353fd39f7d0ab611a275daebe3c
Andreas Zweili <andreas@zweili.ch>
2022-01-25 06:40:25 +0100

    open additional ports for plex

--- modules/plex/default.nix
+++ modules/plex/default.nix
@@ -1,6 +1,17 @@
 { ... }:
 {
   networking = {
-    firewall.allowedTCPPorts = [ 32400 ];
+    firewall.allowedTCPPorts = [
+      32400 # Web Interface/ Remote Access
+    ];
+    firewall.allowedUDPPorts = [
+      1900 # DLNA
+      5353 # Bonjour/Avahi
+      32410 # GDM network discovery
+      32412 # GDM network discovery
+      32413 # GDM network discovery
+      32414 # GDM network discovery
+      32469 # Plex DLNA Server
+    ];
   };
 }

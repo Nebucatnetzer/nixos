45e718f25958618fce935fff7a13dc9b70fdb677
Andreas Zweili <andreas@zweili.ch>
2022-01-26 08:30:26 +0100

    formating

--- home-manager/software/evince/default.nix
+++ home-manager/software/evince/default.nix
@@ -6,10 +6,10 @@
   xdg.mimeApps = {
     enable = true;
     associations.added = {
-      "application/pdf" = ["org.gnome.Evince.desktop"];
+      "application/pdf" = [ "org.gnome.Evince.desktop" ];
     };
     defaultApplications = {
-      "application/pdf" = ["org.gnome.Evince.desktop"];
+      "application/pdf" = [ "org.gnome.Evince.desktop" ];
     };
   };
 }

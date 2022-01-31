5cc076232555b5cfeb58ce8f973d4fadd9d82d8b
Andreas Zweili <andreas@zweili.ch>
2022-01-31 12:28:59 +0100

    update scripts

---
+++ scripts/update_single_machine.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+rsa_key="~/.nixos/secrets/ssh_keys/ansible/ansible.key"
+export NIX_SSHOPTS="-t -i " + rsa_key
+
+host=$1
+fqdn="${host}.2li.local"
+nixos-rebuild switch --use-remote-sudo --build-host localhost --target-host $fqdn --flake ".#${host}"

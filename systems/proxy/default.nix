3dbe3ceef6a29bcf2fee7830faa3b9bcc5ce59e4
Andreas Zweili <andreas@zweili.ch>
2022-02-28 21:52:41 +0100

    remove restic from the proxy

--- systems/proxy/default.nix
+++ systems/proxy/default.nix
@@ -6,9 +6,6 @@
       ip = "10.7.89.100";
       inherit inputs;
     })
-    (import "${inputs.self}/modules/restic-server-client" {
-      time = "21:30"; inherit config custom inputs pkgs;
-    })
     "${inputs.self}/modules/haproxy"
   ];
 }

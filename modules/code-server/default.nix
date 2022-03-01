479bf4b7defc5f28bc9e6e3401997f749d793431
Andreas Zweili <andreas@zweili.ch>
2022-03-01 23:15:21 +0100

    another try

--- modules/code-server/default.nix
+++ modules/code-server/default.nix
@@ -16,7 +16,9 @@
     extraEnvironment = {
       HOME = "/home/${custom.username}";
       EXTENSIONS_GALLERY = ''
-        { "serviceUrl": "https://marketplace.visualstudio.com/_apis/public/gallery"}'';
+        {"serviceUrl": "https://marketplace.visualstudio.com/_apis/public/gallery",
+        "cacheUrl": "https://vscode.blob.core.windows.net/gallery/index",
+        "itemUrl": "https://marketplace.visualstudio.com/items"}'';
     };
   };
   networking.firewall.allowedTCPPorts = [ 4444 ];

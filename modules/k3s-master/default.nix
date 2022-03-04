e65d75d81a5d0460240e42e8881b621b2c4babe5
Andreas Zweili <andreas@zweili.ch>
2022-03-04 17:33:15 +0100

    add environment variable

--- modules/k3s-master/default.nix
+++ modules/k3s-master/default.nix
@@ -9,5 +9,8 @@
     pkgs.kubectl
     pkgs.kubernetes-helm
   ];
+  environment.variables = {
+    KUBECONFIG = "/etc/rancher/k3s/k3s.yaml";
+  };
   networking.firewall.allowedTCPPorts = [ 6443 ];
 }

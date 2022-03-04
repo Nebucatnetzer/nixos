a02c6a70971ecbd85f5fa649b9c035a1ccf097a6
Andreas Zweili <andreas@zweili.ch>
2022-03-04 16:28:01 +0100

    add hardcoded path to token
    I later have to change this to something like agenix path or so.

--- modules/k3s-node/default.nix
+++ modules/k3s-node/default.nix
@@ -4,6 +4,6 @@
     enable = true;
     role = "agent";
     serverAddr = "https://10.7.89.130:6443";
-    tokenFile = "${inputs.self}/secrets/passwords/k3s_token.key";
+    tokenFile = "/etc/k3s/token.key";
   };
 }

653db0030272dacd8e35fa1e5e13127b23973415
Andreas Zweili <andreas@zweili.ch>
2022-03-07 23:33:41 +0100

    more tries

--- modules/nomad-client/default.nix
+++ modules/nomad-client/default.nix
@@ -5,9 +5,13 @@
     settings = {
       client = {
         enabled = true;
+        servers = [ "10.7.89.140:4647" ];
       };
     };
     enableDocker = true;
   };
-  networking.firewall.allowedTCPPorts = [ 4646 ];
+  networking.firewall = {
+    allowedTCPPorts = [ 4646 4647 4648 ];
+    allowedUDPPorts = [ 4648 ];
+  };
 }

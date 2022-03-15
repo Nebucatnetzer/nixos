3dff1cf402161e4d6d596fb78028de5c22cae1ff
Andreas Zweili <andreas@zweili.ch>
2022-03-15 17:05:10 +0100

    add alias for raspi firmware upgrades

--- systems/raspi4/default.nix
+++ systems/raspi4/default.nix
@@ -21,4 +21,15 @@
   documentation.enable = false;
   documentation.nixos.enable = false;
   programs.command-not-found.enable = false;
+
+  environment.systemPackages = with pkgs; [
+    raspberrypi-eeprom
+  ];
+
+  environment.shellAliases = {
+    raspi-firmware-update = ''
+      sudo mount /dev/disk/by-label/FIRMWARE /mnt && \
+      BOOTFS=/mnt FIRMWARE_RELEASE_STATUS=stable sudo -E rpi-eeprom-update -d -a
+    '';
+  };
 }

9fdbf4b030aa7885bd0aad1b66fc2efd89800bce
Andreas Zweili <andreas@zweili.ch>
2022-03-19 12:32:44 +0100

    newest kernel doesn't work

--- systems/raspi4/default.nix
+++ systems/raspi4/default.nix
@@ -26,7 +26,6 @@
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.efiInstallAsRemovable = true;
   boot.loader.grub.device = "nodev";
-  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   hardware.raspberry-pi."4".fkms-3d.enable = true;
   hardware.raspberry-pi."4".audio.enable = true;

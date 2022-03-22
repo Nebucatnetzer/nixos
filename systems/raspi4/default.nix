268193a9562885e95c1a7b27f329a0620344e996
Andreas Zweili <andreas@zweili.ch>
2022-03-22 15:39:26 +0100

    disable EFI vars

--- systems/raspi4/default.nix
+++ systems/raspi4/default.nix
@@ -26,6 +26,7 @@
     "reset_raspberrypi"
     "sdhci_pci"
   ]
+  boot.loader.efi.canTouchEfiVariables = false;
   #boot.loader.grub.enable = true;
   #boot.loader.grub.efiSupport = true;
   #boot.loader.grub.efiInstallAsRemovable = true;

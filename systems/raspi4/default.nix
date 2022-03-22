693a33b7fb31df6b889452a1b892a4aa9d12b574
Andreas Zweili <andreas@zweili.ch>
2022-03-22 16:00:30 +0100

    add missing semi-colon

--- systems/raspi4/default.nix
+++ systems/raspi4/default.nix
@@ -25,7 +25,7 @@
   boot.initrd.availableKernelModules = [
     "reset_raspberrypi"
     "sdhci_pci"
-  ]
+  ];
   boot.loader.efi.canTouchEfiVariables = false;
   #boot.loader.grub.enable = true;
   #boot.loader.grub.efiSupport = true;

7869c0360fecce3aad53a6eb090239d37862d60a
Andreas Zweili <andreas@zweili.ch>
2022-05-17 22:17:50 +0200

    Set missing ACME settings

--- modules/nginx-acme/default.nix
+++ modules/nginx-acme/default.nix
@@ -3,6 +3,10 @@
   networking.firewall.allowedTCPPorts = [
     443
   ];
+  security.acme = {
+    acceptTerms = true;
+    email = "admin+acme@zweili.ch";
+  };
   services.nginx = {
     enable = true;
     recommendedProxySettings = true;

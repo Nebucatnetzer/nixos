aade91330965b44a2c133cdd445574adf8e3b65a
Andreas Zweili <andreas@zweili.ch>
2022-05-31 22:41:15 +0200

    Migrate MariaDB to ttrss

--- systems/ttrss/default.nix
+++ systems/ttrss/default.nix
@@ -6,12 +6,13 @@
       ip = "10.7.89.115";
       inherit inputs;
     })
-    (import "${inputs.self}/modules/restic-server-client" {
-      time = "23:00"; inherit config custom inputs pkgs;
-    })
     (import "${inputs.self}/modules/nginx-acme" {
       domain = "ttrss.2li.ch"; inherit inputs;
     })
+    (import "${inputs.self}/modules/restic-server-mysql-client" {
+      time = "23:00"; inherit config custom inputs pkgs;
+    })
     "${inputs.self}/modules/docker"
+    "${inputs.self}/modules/mariadb"
   ];
 }

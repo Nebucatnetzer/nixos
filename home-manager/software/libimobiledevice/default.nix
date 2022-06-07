d4a2eb3334e544c1f676a84e70884186d604b3d8
Andreas Zweili <andreas@zweili.ch>
2022-06-07 21:41:19 +0200

    Use bzip2 for the iDevice backups

--- home-manager/software/libimobiledevice/default.nix
+++ home-manager/software/libimobiledevice/default.nix
@@ -8,14 +8,14 @@
       backup_dir=~/Downloads/$(date -I)_iphone &&
       mkdir -p $backup_dir &&
       idevicebackup2 backup $backup_dir &&
-      zip -r $backup_dir.zip $backup_dir &&
+      tar cjf $backup_dir.tar.bz2 $backup_dir &&
       rm -r $backup_dir
     '';
     ipad-backup = ''
       backup_dir=~/Downloads/$(date -I)_ipad &&
       mkdir -p $backup_dir &&
       idevicebackup2 backup $backup_dir &&
-      zip -r $backup_dir.zip $backup_dir &&
+      tar cjf $backup_dir.tar.bz2 $backup_dir &&
       rm -r $backup_dir
     '';
   };

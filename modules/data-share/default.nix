eb2c85ef068dc1a3e8541289bf96831e7dbeddf2
Andreas Zweili <andreas@zweili.ch>
2022-06-07 23:32:48 +0200

    Add noatime to the data-share

--- modules/data-share/default.nix
+++ modules/data-share/default.nix
@@ -6,6 +6,6 @@
   fileSystems."/mnt/data" = {
     device = "10.7.89.108:server_data";
     fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=300" ];
+    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=300" "noatime" ];
   };
 }

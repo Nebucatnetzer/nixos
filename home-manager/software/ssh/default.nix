aa7624000555e638a074961abdf9b6bfcecb29b5
Andreas Zweili <andreas@zweili.ch>
2022-06-29 21:40:40 +0200

    Fix indentation for SSH config

--- home-manager/software/ssh/default.nix
+++ home-manager/software/ssh/default.nix
@@ -3,18 +3,18 @@
     enable = true;
     extraConfig = ''
       Host nixos.2li.local
-      StrictHostKeyChecking no
-      UserKnownHostsFile /dev/null
-      User nixos
-      LogLevel QUIET
+        StrictHostKeyChecking no
+        UserKnownHostsFile /dev/null
+        User nixos
+        LogLevel QUIET
 
       Host *.2li.local
-      User andreas
-      IdentityFile ~/.nixos/secrets/ssh_keys/ansible/ansible.key
+        User andreas
+        IdentityFile ~/.nixos/secrets/ssh_keys/ansible/ansible.key
 
       Host 10.7.89.*
-      User andreas
-      IdentityFile ~/.nixos/secrets/ssh_keys/ansible/ansible.key
+        User andreas
+        IdentityFile ~/.nixos/secrets/ssh_keys/ansible/ansible.key
     '';
   };
 }

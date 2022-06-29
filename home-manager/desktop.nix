36464ddd7620b2facad0a09c922dca868cb7dd52
Andreas Zweili <andreas@zweili.ch>
2022-06-29 22:24:27 +0200

    Add functions to open ports temporarily

--- home-manager/desktop.nix
+++ home-manager/desktop.nix
@@ -49,6 +49,17 @@
 
   programs.bash = {
     enable = true;
+    bashrcExtra = ''
+      open-port() {
+        local port=$1
+        sudo iptables -A INPUT -p tcp --dport $port -j ACCEPT
+      }
+
+      close-port() {
+        local port=$1
+        sudo iptables -D INPUT -p tcp --dport $port -j ACCEPT
+      }
+    '';
   };
 
   services.nextcloud-client = {

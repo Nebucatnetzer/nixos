eb93c9137aa78a4403957283f344567aa783e914
Andreas Zweili <andreas@zweili.ch>
2022-08-08 11:44:00 +0200

    Add the compress-pdf script

--- modules/scripts/default.nix
+++ modules/scripts/default.nix
@@ -4,7 +4,19 @@ let
     "remove-special-characters"
     "${builtins.readFile ./remove_special_characters.sh}";
 
+  compress-pdf = pkgs.writeScriptBin "compress-pdf" ''
+    ${pkgs.ghostscript}/bin/gs -sDEVICE=pdfwrite \
+        -dCompatibilityLevel=1.5 \
+        -dNOPAUSE \
+        -dQUIET \
+        -dBATCH \
+        -sOutputFile=compressed_$1 $1'';
+
 in
 {
-  environment.systemPackages = [ remove-special-characters ];
+  environment.systemPackages = [
+    remove-special-characters
+    compress-pdf
+  ];
 }
+

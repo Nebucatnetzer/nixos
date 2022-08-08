05185aa8d76ee57809c1bab192d3ac5aad5e8ff1
Andreas Zweili <andreas@zweili.ch>
2022-08-08 11:41:26 +0200

    Use writeScriptBin
    Other than writeShellScriptBin writeScriptBin doesn't replace the Shebang.
    I probably have to rewrite this at some point but for the moment it's easier
    this way.

--- modules/scripts/default.nix
+++ modules/scripts/default.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 let
-  remove-special-characters = pkgs.writeShellScriptBin "${builtins.readFile ./remove_special_characters.sh}";
+  remove-special-characters = pkgs.writeScriptBin
+    "remove-special-characters"
+    "${builtins.readFile ./remove_special_characters.sh}";
+
 in
 {
   environment.systemPackages = [ remove-special-characters ];

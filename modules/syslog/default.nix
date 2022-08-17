52785d4d54db80599337d2f979dd6a9c569d371c
Andreas Zweili <andreas@zweili.ch>
2022-08-17 20:40:05 +0200

    Replace the default syslog config

--- modules/syslog/default.nix
+++ modules/syslog/default.nix
@@ -2,7 +2,7 @@
 {
   services.rsyslogd = {
     enable = true;
-    extraConfig = ''
+    defaultConfig = ''
       *.*  action(type="omfwd" target="10.7.89.108" port="514" protocol="udp"
                   action.resumeRetryCount="100"
                   queue.type="linkedList" queue.size="10000")

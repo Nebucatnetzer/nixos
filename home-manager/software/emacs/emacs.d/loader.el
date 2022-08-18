c40ad704401f5204964b846f049f2f1ca5e9f78a
Andreas Zweili <andreas@zweili.ch>
2022-08-18 21:04:11 +0200

    Move the OS detection into a separate file

--- home-manager/software/emacs/emacs.d/loader.el
+++ home-manager/software/emacs/emacs.d/loader.el
@@ -1,18 +1,5 @@
+(load-file (config-path "detect_os.el"))
 (load-file "~/.emacs.d/variables.el")
-
-(defun is-mac-p ()
-  (eq system-type 'darwin))
-
-(defun is-linux-p ()
-  (eq system-type 'gnu/linux))
-
-(defun is-windows-p ()
-  (or (eq system-type 'ms-dos)
-      (eq system-type 'windows-nt)
-      (eq system-type 'cygwin)))
-
-(defun is-bsd-p ()
-  (eq system-type 'gnu/kfreebsd))
 
 ;; MELPA
 (add-to-list 'package-archives

f0893ae5c96065b68c9b21c3692d6ef761b958ee
Andreas Zweili <andreas@zweili.ch>
2022-08-18 21:22:26 +0200

    Add file for UI changes

--- home-manager/software/emacs/emacs.d/lib/loader.el
+++ home-manager/software/emacs/emacs.d/lib/loader.el
@@ -1,10 +1,3 @@
-;; smooth scrolling
-(use-package smooth-scrolling
-  :ensure t
-  :config
-  (setq scroll-margin 1
-        scroll-conservatively 9999
-        scroll-step 1))
 
 (use-package deft
   :ensure t
---
+++ home-manager/software/emacs/emacs.d/lib/ui.el
@@ -0,0 +1,7 @@
+;; smooth scrolling
+(use-package smooth-scrolling
+  :ensure t
+  :config
+  (setq scroll-margin 1
+        scroll-conservatively 9999
+        scroll-step 1))

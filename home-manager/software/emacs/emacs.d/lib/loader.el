add3d95e81d6a91decbe50fdd3f24bbef82e5f9c
Andreas Zweili <andreas@zweili.ch>
2022-08-20 16:36:31 +0200

    Add a file for counsel

--- home-manager/software/emacs/emacs.d/lib/loader.el
+++ home-manager/software/emacs/emacs.d/lib/loader.el
@@ -60,26 +60,6 @@
   :config
   (amx-mode t))
 
-(use-package counsel
-  :ensure t
-  :config
-  (ivy-mode 1)
-  (counsel-mode 1)
-  (setq ivy-use-virtual-buffers t)
-  (setq ivy-count-format "(%d/%d) ")
-  (setq ivy-initial-inputs-alist nil)
-  (with-eval-after-load 'org
-    (define-key org-mode-map (kbd "C-c C-q") #'counsel-org-tag))
-  (global-set-key (kbd "C-c v") 'ivy-push-view)
-  (global-set-key (kbd "C-c V") 'ivy-pop-view)
-  ;; (global-set-key (kbd "C-s") 'swiper-isearch)
-  (evil-define-key 'normal 'global "/" 'swiper-isearch)
-  (global-set-key (kbd "C-c g") 'counsel-git)
-  (global-set-key (kbd "C-c j") 'counsel-git-grep)
-  (global-set-key (kbd "C-c k") 'counsel-ag)
-  (define-key ivy-minibuffer-map (kbd "S-SPC") (lambda () (interactive) (insert " ")))
-  (global-set-key (kbd "C-c C-r") 'ivy-resume))
-
 (use-package eyebrowse
   :ensure t
   :init

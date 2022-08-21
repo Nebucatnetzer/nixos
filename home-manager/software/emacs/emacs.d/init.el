09476f681faba4b81d011789b298c2c045376c48
Andreas Zweili <andreas@zweili.ch>
2022-08-21 15:19:58 +0200

    Move the file loads into init.el

--- home-manager/software/emacs/emacs.d/init.el
+++ home-manager/software/emacs/emacs.d/init.el
@@ -74,4 +74,49 @@
   (setq emacs-config-dir "~/.nixos/home-manager/software/emacs/emacs.d/lib/")
   (concat emacs-config-dir config))
 
-(load-file "~/.nixos/home-manager/software/emacs/emacs.d/load_file.el")
+;; load specific config files
+;; basic settings
+(load-file "~/.emacs.d/variables.el")
+(load-file (config-path "detect_os.el"))
+(load-file (config-path "evil.el"))
+(load-file (config-path "packages.el"))
+(load-file (config-path "theme.el"))
+(load-file (config-path "ui.el"))
+
+;; keybindings
+(load-file (config-path "hydra.el"))
+(load-file (config-path "keybindings.el"))
+
+;; note taking and writing
+(load-file (config-path "deft.el"))
+(load-file (config-path "latex.el"))
+(load-file (config-path "langtool.el"))
+(load-file (config-path "markdown.el"))
+(load-file (config-path "move_text.el"))
+(load-file (config-path "org_mode.el"))
+(load-file (config-path "org_insert.el"))
+(load-file (config-path "pdf_tools.el"))
+(load-file (config-path "spellcheck.el"))
+
+;; programming
+(load-file (config-path "bug_hunter.el"))
+(load-file (config-path "company.el"))
+(load-file (config-path "direnv.el"))
+(load-file (config-path "format_all.el"))
+(load-file (config-path "magit.el"))
+(load-file (config-path "powershell.el"))
+(load-file (config-path "python.el"))
+(load-file (config-path "webmode.el"))
+(load-file (config-path "yaml.el"))
+
+;; various
+(load-file (config-path "counsel.el"))
+(load-file (config-path "dired.el"))
+(load-file (config-path "eyebrowse.el"))
+(load-file (config-path "keyring_update.el"))
+(load-file (config-path "manuals.el"))
+(load-file (config-path "mu4e.el"))
+(load-file (config-path "treemacs.el"))
+(load-file (config-path "utils.el"))
+(load-file (config-path "window_split.el"))
+(load-file (config-path "yasnippet.el"))

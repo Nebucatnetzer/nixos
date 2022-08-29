eeb48df35a4600ac0c0db59fff260ff8f59444c7
Andreas Zweili <andreas@zweili.ch>
2022-08-29 22:09:05 +0200

    Migrate the libimobiledevice to a normal module
    The reason is because I want to have the usbmuxd service in that config as well.

---
+++ modules/libimobiledevice/default.nix
@@ -0,0 +1,26 @@
+{ custom, pkgs, ... }:
+{
+  services.usbmuxd.enable = true;
+  environment.systemPackages = with pkgs;
+    [
+      libimobiledevice
+    ];
+
+  home-manager.users.${custom.username} = {
+    home.shellAliases = {
+      iphone-backup = ''
+        backup_dir=~/Downloads/$(date -I)_iphone &&
+        mkdir -p $backup_dir &&
+        ${pkgs.libimobiledevice}/bin/idevicebackup2 backup $backup_dir &&
+        ${pkgs.gnutar}/bin/tar cjf $backup_dir.tar.bz2 $backup_dir &&
+        rm -r $backup_dir'';
+
+      ipad-backup = ''
+        backup_dir=~/Downloads/$(date -I)_ipad &&
+        mkdir -p $backup_dir &&
+        ${pkgs.libimobiledevice}/bin/idevicebackup2 backup $backup_dir &&
+        ${pkgs.gnutar}/bin/tar cjf $backup_dir.tar.bz2 $backup_dir &&
+        rm -r $backup_dir'';
+    };
+  };
+}

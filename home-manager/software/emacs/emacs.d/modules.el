d5767fce5afdb5fa957f12e8634a803c7d75ab93
Andreas Zweili <andreas@zweili.ch>
2022-08-31 20:30:37 +0200

    Add a modules.el file
    The idea behind this, is that it is easier to modify my Emacs config when I
    don't have to rebuild the system config all the time. The config itself is
    still under version control so should still be reproducable.

---
+++ home-manager/software/emacs/emacs.d/modules.el
@@ -0,0 +1,44 @@
+(load-file (modules-path "detect_os.el"))
+(load-file (modules-path "dired.el"))
+(load-file (modules-path "packages.el"))
+(load-file (modules-path "theme.el"))
+(load-file (modules-path "ui.el"))
+(load-file (modules-path "utils.el"))
+(load-file (modules-path "window_split.el"))
+
+;; keybindings
+(load-file (modules-path "evil.el"))
+(load-file (modules-path "hydra.el"))
+(load-file (modules-path "keybindings.el"))
+
+;; note taking and writing
+(load-file (modules-path "deft.el"))
+(load-file (modules-path "latex.el"))
+(load-file (modules-path "langtool.el"))
+(load-file (modules-path "markdown.el"))
+(load-file (modules-path "move_text.el"))
+(load-file (modules-path "org_mode.el"))
+(load-file (modules-path "org_insert.el"))
+(load-file (modules-path "pdf_tools.el"))
+(load-file (modules-path "spellcheck.el"))
+
+;; programming
+(load-file (modules-path "bug_hunter.el"))
+(load-file (modules-path "company.el"))
+(load-file (modules-path "direnv.el"))
+(load-file (modules-path "format_all.el"))
+(load-file (modules-path "magit.el"))
+(load-file (modules-path "powershell.el"))
+(load-file (modules-path "python.el"))
+(load-file (modules-path "webmode.el"))
+(load-file (modules-path "yaml.el"))
+
+;; various third party
+(load-file (modules-path "counsel.el"))
+(load-file (modules-path "eyebrowse.el"))
+(load-file (modules-path "keyring_update.el"))
+(load-file (modules-path "manuals.el"))
+(load-file (modules-path "mu4e.el"))
+(load-file (modules-path "treemacs.el"))
+(load-file (modules-path "vterm.el"))
+(load-file (modules-path "yasnippet.el"))

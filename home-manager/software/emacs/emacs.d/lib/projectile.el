8a838371a6b29477bf955f9c59f45c57c04e1a99
Andreas Zweili <andreas@zweili.ch>
2022-08-31 22:47:24 +0200

    Add ~/.nixos to projectile

--- home-manager/software/emacs/emacs.d/lib/projectile.el
+++ home-manager/software/emacs/emacs.d/lib/projectile.el
@@ -3,7 +3,9 @@
   :init
   (projectile-mode +1)
   :config
-  (setq projectile-project-search-path '("~/git_repos/projects/" "~/git_repos/work/"))
+  (setq projectile-project-search-path '(("~/.nixos" . 1)
+                                         "~/git_repos/projects/"
+                                         "~/git_repos/work/"))
   :bind (:map projectile-mode-map
               ("M-p" . projectile-command-map)
               ("C-c p" . projectile-command-map)))

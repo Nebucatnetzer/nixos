a41593f712c82edf351649a0746d7d704556fff4
Andreas Zweili <andreas@zweili.ch>
2022-09-07 17:27:56 +0200

    Disable bold font in vterm

--- home-manager/software/emacs/emacs.d/lib/vterm.el
+++ home-manager/software/emacs/emacs.d/lib/vterm.el
@@ -1,7 +1,8 @@
 (use-package vterm
   :ensure t
   :config
-  (setq vterm-kill-buffer-on-exit t)
+  (setq vterm-kill-buffer-on-exit t
+        vterm-disable-bold t)
   (global-set-key (kbd "M RET") 'vterm)
   (evil-set-initial-state 'vterm-mode 'emacs))
 

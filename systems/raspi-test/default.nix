ee613016bd8c8eda169c088b1af02c5bf33945f2
Andreas Zweili <andreas@zweili.ch>
2022-09-07 22:06:17 +0200

    Enable NGINX on raspi-test

--- systems/raspi-test/default.nix
+++ systems/raspi-test/default.nix
@@ -5,6 +5,10 @@
       ip = "10.7.89.150";
       inherit hostname inputs pkgs;
     })
+    (import "${inputs.self}/modules/nginx-acme" {
+      domain = "test.2li.ch";
+      inherit inputs;
+    })
     "${inputs.self}/modules/docker"
     "${inputs.self}/modules/tmux"
   ];

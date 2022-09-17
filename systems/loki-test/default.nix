01cb2961b625f29d0695767d19e09b6286756360
Andreas Zweili <andreas@zweili.ch>
2022-09-17 17:30:37 +0200

    Add pihole and unbound role to loki-test

--- systems/loki-test/default.nix
+++ systems/loki-test/default.nix
@@ -6,7 +6,9 @@
       inherit hostname inputs pkgs;
     })
     "${inputs.self}/modules/docker"
+    "${inputs.self}/modules/pihole"
     "${inputs.self}/modules/router"
     "${inputs.self}/modules/tmux"
+    "${inputs.self}/modules/unbound"
   ];
 }

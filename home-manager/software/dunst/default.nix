67580a211c88c77cdab46d230333d03accf3f991
Andreas Zweili <andreas@zweili.ch>
2022-09-17 20:38:45 +0200

    Clean up the dunst config

--- home-manager/software/dunst/default.nix
+++ home-manager/software/dunst/default.nix
@@ -6,18 +6,14 @@
       global = {
         font = "Source Code Pro 11";
         markup = "yes";
-        plain_text = "no";
-        format = "<b>%s</b>\n%b";
+        format = "<b>%s</b>\\n%b";
         sort = "no";
         indicate_hidden = "yes";
         alignment = "center";
-        bounce_freq = 0;
         show_age_threshold = -1;
         word_wrap = "yes";
         ignore_newline = "no";
         stack_duplicates = "yes";
-        hide_duplicates_count = "yes";
-        geometry = "300x50-15+49";
         shrink = "no";
         transparency = 5;
         idle_threshold = 0;
@@ -31,7 +27,6 @@
         padding = 6;
         horizontal_padding = 6;
         separator_color = "frame";
-        startup_notification = "false";
         browser = "firefox - new-tab";
         icon_position = "off";
         max_icon_size = 80;
@@ -42,21 +37,18 @@
         frame_color = "#3B7C87";
         foreground = "#3B7C87";
         background = "#191311";
-        #background = "#2B313C";
         timeout = 4;
       };
       urgency_normal = {
         frame_color = "#5B8234";
         foreground = "#5B8234";
         background = "#191311";
-        #background = "#2B313C";
         timeout = 6;
       };
       urgency_critical = {
         frame_color = "#B7472A";
         foreground = "#B7472A";
         background = "#191311";
-        #background = "#2B313C";
         timeout = 8;
       };
     };

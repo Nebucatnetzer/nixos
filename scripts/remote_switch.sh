4289a62f239ba3574dd5061902b3e65708005247
Andreas Zweili <andreas@zweili.ch>
2022-10-14 08:40:33 +0200

    Remove unused code

--- scripts/remote_switch.sh
+++ scripts/remote_switch.sh
@@ -20,9 +20,6 @@ do
     fqdn="$host.2li.local"
     echo $fqdn
     nixos-rebuild switch -j auto --use-remote-sudo --build-host localhost --target-host $fqdn --flake ".#$host"
-    if [ $host == "nixos-management" ]; then
-        continue
-    fi
     echo "reboot $fqdn"
     ssh -i $rsa_key $fqdn 'sudo reboot'
     echo

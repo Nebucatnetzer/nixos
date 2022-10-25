cd06a036bc2093e1ad645bfff3dcb1923aab6adf
Andreas Zweili <zweili@contria.com>
2022-10-25 13:47:55 +0200

    Use black as the default formatter

--- home-manager/software/emacs/emacs.d/lib/format_all.el
+++ home-manager/software/emacs/emacs.d/lib/format_all.el
@@ -60,7 +60,7 @@
           ("PHP" prettier)
           ("Protocol Buffer" clang-format)
           ("PureScript" purty)
-          ("Python" autopep8)
+          ("Python" black)
           ("R" styler)
           ("Reason" bsrefmt)
           ("ReScript" rescript)

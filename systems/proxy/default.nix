591903f1ac2c3d77bc7b85d0b6208d74655d41ca
Andreas Zweili <andreas@zweili.ch>
2022-11-02 11:32:26 +0100

    Move the nginx config to rss-bridge

--- systems/proxy/default.nix
+++ systems/proxy/default.nix
@@ -62,15 +62,6 @@
           proxyWebsockets = true; # needed if you need to use WebSocket
         };
       };
-      "rss-bridge.2li.ch" = {
-        enableACME = true;
-        forceSSL = true;
-        listen = [{ port = 4433; addr = "127.0.0.1"; ssl = true; }];
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:8082";
-          proxyWebsockets = true; # needed if you need to use WebSocket
-        };
-      };
     };
   };
 }

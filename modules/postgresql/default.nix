6ae6fd0f5bd4316e7fea62daadbfabce0d46a9a0
Andreas Zweili <andreas@zweili.ch>
2022-11-02 21:44:41 +0100

    Limit the listen addresses for PostgreSQL

--- modules/postgresql/default.nix
+++ modules/postgresql/default.nix
@@ -1,8 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services.postgresql = {
     enable = true;
     enableTCPIP = true;
+    settings.listen_adresses = pkgs.lib.mkForce "127.0.0.1,172.17.0.1";
   };
   networking.firewall.extraCommands = "iptables -A INPUT -p tcp --destination-port 3306 -s 172.16.0.0/12 -j ACCEPT";
 }

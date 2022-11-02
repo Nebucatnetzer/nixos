7b6e254ea3167e4b03214c279638d62180d906eb
Andreas Zweili <andreas@zweili.ch>
2022-11-02 20:54:02 +0100

    Get all the hosts programmatically from the flake

--- scripts/remote_switch.sh
+++ scripts/remote_switch.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 
-hosts=(
- "mail"
- "nextcloud"
- "plex"
- "management"
- "restic-server"
- "ttrss"
- "git"
- "proxy"
- "pihole"
+hosts=($(echo `nix eval .#nixosConfigurations --apply 'pkgs: builtins.concatStringsSep " " (builtins.attrNames pkgs)'` | xargs ))
+skip=(
+    "gwyn"
+    "loki-test"
+    "desktop-vm"
 )
 
 rsa_key="$HOME/.nixos/secrets/ssh_keys/ansible/ansible.key"
@@ -17,6 +12,10 @@ export NIX_SSHOPTS="-t -i $rsa_key"
 
 for host in "${hosts[@]}"
 do
+    # Check if the host is in the skip list
+    if [[ " ${skip[*]} " =~ " ${host} " ]];then
+        continue
+    fi
     fqdn="$host.2li.local"
     echo $fqdn
     nixos-rebuild switch -j auto --use-remote-sudo --build-host localhost --target-host $fqdn --flake ".#$host"

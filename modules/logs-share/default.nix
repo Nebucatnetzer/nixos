e6963213c5eea2d882fa53cbe902e8876f69e048
Andreas Zweili <andreas@zweili.ch>
2022-11-04 14:31:51 +0100

    Mount logs read-only

--- modules/logs-share/default.nix
+++ modules/logs-share/default.nix
@@ -6,6 +6,6 @@
   fileSystems."/mnt/server_logs" = {
     device = "10.7.89.108:logs";
     fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=300" "noatime" "nfsvers=4.0" ];
+    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=300" "noatime" "nfsvers=4.0" "ro" ];
   };
 }

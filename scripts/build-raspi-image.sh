1df48d32c975f6716cb700bf6b40f0e3be917c8f
Andreas Zweili <andreas@zweili.ch>
2022-11-05 20:29:18 +0100

    Extend the image build script

--- scripts/build-raspi-image.sh
+++ scripts/build-raspi-image.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 host=$1
+image=~/Downloads/$host.img
 
 cd ~/.nixos/
 
 nix build .#images.$host &&
-unzstd result/sd-image/*.img.zst -o ~/Downloads/$host.img
+unzstd result/sd-image/*.img.zst -o $image
+touch $image
+chmod 644 $image
+rm result

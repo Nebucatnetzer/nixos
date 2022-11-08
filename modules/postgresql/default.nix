8e24754b6cb3d95557d4ef359712e769e62453d6
Andreas Zweili <andreas@zweili.ch>
2022-11-08 23:56:03 +0100

    Correct Postgres port in firewall

--- modules/postgresql/default.nix
+++ modules/postgresql/default.nix
@@ -5,5 +5,5 @@
     enableTCPIP = true;
     settings.listen_adresses = pkgs.lib.mkForce "127.0.0.1,172.17.0.1";
   };
-  networking.firewall.extraCommands = "iptables -A INPUT -p tcp --destination-port 3306 -s 172.16.0.0/12 -j ACCEPT";
+  networking.firewall.extraCommands = "iptables -A INPUT -p tcp --destination-port 5432 -s 172.16.0.0/12 -j ACCEPT";
 }

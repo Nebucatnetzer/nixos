9d2a8a7e9825430182bf5cd56e5fad27abbdb0e2
Andreas Zweili <andreas@zweili.ch>
2022-11-09 23:42:04 +0100

    Automatically switch audio outputs

--- modules/pipewire/default.nix
+++ modules/pipewire/default.nix
@@ -6,6 +6,12 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    config.pipewire = {
+      "context.exec" = [{
+        path = "pactl";
+        args = "load-module module-switch-on-connect";
+      }];
+    };
   };
   home-manager.users.${custom.username} = {
     services.easyeffects.enable = true;

5b52e6cc0116ebe797d0d8d9bb9d590a90f85c94
Andreas Zweili <andreas@zweili.ch>
2022-11-09 19:12:36 +0100

    Remove listen parameter from rss-bridge nginx config
    This was only necessary because it was running behind haproxy before.

--- systems/ttrss/default.nix
+++ systems/ttrss/default.nix
@@ -18,7 +18,6 @@ in
       "rss-bridge.2li.ch" = {
         enableACME = true;
         forceSSL = true;
-        listen = [{ port = 4433; addr = "127.0.0.1"; ssl = true; }];
         locations."/" = {
           proxyPass = "http://127.0.0.1:8082";
           proxyWebsockets = true; # needed if you need to use WebSocket

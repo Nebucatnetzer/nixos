69002ac0e13ab5917348e8a1a8d4a59c5d94982d
Andreas Zweili <andreas@zweili.ch>
2022-11-09 01:20:40 +0100

    Fix parameter in postgres

--- modules/postgresql/default.nix
+++ modules/postgresql/default.nix
@@ -3,7 +3,7 @@
   services.postgresql = {
     enable = true;
     enableTCPIP = true;
-    settings.listen_adresses = pkgs.lib.mkForce "127.0.0.1,172.17.0.1";
+    settings.listen_addresses = pkgs.lib.mkForce "127.0.0.1,172.17.0.1";
   };
   networking.firewall.extraCommands = "iptables -A INPUT -p tcp --destination-port 5432 -s 172.16.0.0/12 -j ACCEPT";
 }

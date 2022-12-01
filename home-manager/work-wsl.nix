08a83e1d9b7afcf16a0880484fa35d09279064d2
Andreas Zweili <zweili@contria.com>
2022-12-01 16:10:09 +0100

    Add initial config for Nix in WSL

--- home-manager/work-wsl.nix
+++ home-manager/work-wsl.nix
@@ -21,6 +21,25 @@
     nixpkgs-fmt
     mosh
   ];
+  nix = {
+    package = pkgs.nix;
+    settings = {
+      connect-timeout = 5;
+      log-lines = 25;
+      experimental-features = "nix-command flakes";
+      fallback = true;
+      warn-dirty = false;
+      auto-optimise-store = true;
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      ];
+    };
+  };
 
   programs.bash = {
     enable = true;

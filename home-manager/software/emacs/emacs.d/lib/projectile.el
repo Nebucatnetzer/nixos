49c23cc6632498ae3c723921a0108df328a1bd18
Andreas Zweili <andreas@zweili.ch>
2022-12-06 22:02:22 +0100

    Use ivy in projectile

--- home-manager/software/emacs/emacs.d/lib/projectile.el
+++ home-manager/software/emacs/emacs.d/lib/projectile.el
@@ -5,6 +5,7 @@
   (setq projectile-project-search-path '(("~/.nixos" . 1)
                                          "~/git_repos/projects/"
                                          "~/git_repos/work/")
+        projectile-completion-system 'ivy
         projectile-switch-project-action #'projectile-dired)
   :bind (:map projectile-mode-map
               ("M-p" . projectile-command-map)))

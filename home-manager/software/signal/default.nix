e5b8f43e9b39a777edb943979097a82704bf04ef
Andreas Zweili <andreas@zweili.ch>
2023-01-09 19:12:54 +0100

    Start signal with the tray icon

--- home-manager/software/signal/default.nix
+++ home-manager/software/signal/default.nix
@@ -5,4 +5,16 @@
   ];
 
   home.file.".config/qtile/autostart.d/signal.sh".source = ./signal.sh;
+  xdg.desktopEntries = {
+    signal = {
+      name = "Signal";
+      exec = "${pkgs.unstable.signal-desktop}/bin/signal-desktop --use-tray-icon --no-sandbox %U";
+      terminal = false;
+      type = "Application";
+      icon = "signal-desktop";
+      comment = "Private messaging from your desktop";
+      mimeType = [ "x-scheme-handler/sgnl" "x-scheme-handler/signalcaptcha" ];
+      categories = [ "Network" "InstantMessaging" "Chat" ];
+    };
+  };
 }

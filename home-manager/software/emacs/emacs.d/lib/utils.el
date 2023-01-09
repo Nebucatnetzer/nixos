d12b40ff37ac717dfbb7dba836a0865983a0d448
Andreas Zweili <andreas@zweili.ch>
2023-01-09 18:15:50 +0100

    Add a function to move files in emacs

--- home-manager/software/emacs/emacs.d/lib/utils.el
+++ home-manager/software/emacs/emacs.d/lib/utils.el
@@ -32,3 +32,22 @@
   (interactive)
   (set-window-dedicated-p (selected-window)
                           (not (window-dedicated-p (selected-window)))))
+
+;; taken from here: https://zck.org/emacs-move-file
+(defun move-file (new-location)
+  "Write this file to NEW-LOCATION, and delete the old one."
+  (interactive (list (expand-file-name
+                      (if buffer-file-name
+                          (read-file-name "Move file to: ")
+                        (read-file-name "Move file to: "
+                                        default-directory
+                                        (expand-file-name (file-name-nondirectory (buffer-name))
+                                                          default-directory))))))
+  (when (file-exists-p new-location)
+    (delete-file new-location))
+  (let ((old-location (expand-file-name (buffer-file-name))))
+    (write-file new-location t)
+    (when (and old-location
+               (file-exists-p new-location)
+               (not (string-equal old-location new-location)))
+      (delete-file old-location))))

3d05553f336c45271525965f2bcf1c60f85a0afa
Andreas Zweili <andreas@zweili.ch>
2023-01-19 18:28:31 +0100

    Jump to dired when switching projects

--- home-manager/software/emacs/emacs.d/lib/projectile.el
+++ home-manager/software/emacs/emacs.d/lib/projectile.el
@@ -8,5 +8,6 @@
         projectile-completion-system 'ivy
         projectile-switch-project-action #'projectile-dired)
   (global-set-key (kbd "C-c g") 'projectile-find-file)
+  (setq projectile-switch-project-action #'projectile-dired)
   :bind (:map projectile-mode-map
               ("C-x p" . projectile-command-map)))

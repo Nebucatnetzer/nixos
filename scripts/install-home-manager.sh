fa7079fa7ac66350d8deb202fe7828523af63ee6
Andreas Zweili <zweili@contria.com>
2023-02-10 09:28:56 +0100

    Update home-manager script

--- scripts/install-home-manager.sh
+++ scripts/install-home-manager.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+# 1. install Nix in single user mode
+. $HOME/.nix-profile/etc/profile.d/nix.sh
+mkdir -p ~/.config/nix
+echo "experimental-features = nix-command flakes" > ~/.config/nix/nix.conf
 nix-shell '<home-manager>' -A install
 rm ~/.config/nixpkgs/home.nix
-ln -s $(pwd)/.nixos/flake.nix /home/$USER/.config/nixpkgs/flake.nix
-mkdir -p ~/.config/nix
-echo "experimental-features = nix-command flakes" > ~/.config/nix/nix.con
+ln -s $HOME/.nixos/flake.nix $HOME/.config/nixpkgs/flake.nix
 home-manager switch

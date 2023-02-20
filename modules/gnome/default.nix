bc155e00c25a6ab5c5ab0f6d8a1482413dd6e2ff
Andreas Zweili <andreas@zweili.ch>
2023-02-20 12:06:45 +0100

    Allow to start applications on the nvidia GPU from Gnome applications

--- modules/gnome/default.nix
+++ modules/gnome/default.nix
@@ -1,12 +1,14 @@
 { pkgs, ... }: {
+  services.switcherooControl.enable = true;
   services.touchegg.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
   services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
+  services.xserver.desktopManager.gnome.enable = true;
   environment = {
     systemPackages = with pkgs; [
       gnome.gnome-tweaks
       gnomeExtensions.appindicator
       gnomeExtensions.gtk-title-bar
+      gnomeExtensions.switcheroo
       gnomeExtensions.x11-gestures
     ];
     gnome.excludePackages = (with pkgs; [

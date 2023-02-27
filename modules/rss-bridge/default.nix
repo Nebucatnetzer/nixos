e8d421f9ef8b9ebef7c6cc93abdc86d64b066be4
Andreas Zweili <andreas@zweili.ch>
2023-02-27 09:58:56 +0100

    Add SHA sum to rss-brige
    rss-bridge doesn't have version numbers.

--- modules/rss-bridge/default.nix
+++ modules/rss-bridge/default.nix
@@ -6,7 +6,7 @@ in
   virtualisation.oci-containers = {
     backend = "docker";
     containers."rss-bridge" = {
-      image = "rssbridge/rss-bridge:latest";
+      image = "rssbridge/rss-bridge@sha256:7683bbdb62a97cf252f66ddc6df0fcf5250052a232e150086e8778c937853320";
       autoStart = true;
       ports = [
         "8082:80"

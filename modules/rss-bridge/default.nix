8e6c3017c2a8a08797c0353e37bd48a34512990c
Andreas Zweili <andreas@zweili.ch>
2023-02-27 11:44:44 +0100

    Specify the correct SHA sum for ARM64

--- modules/rss-bridge/default.nix
+++ modules/rss-bridge/default.nix
@@ -6,7 +6,7 @@ in
   virtualisation.oci-containers = {
     backend = "docker";
     containers."rss-bridge" = {
-      image = "rssbridge/rss-bridge@sha256:7683bbdb62a97cf252f66ddc6df0fcf5250052a232e150086e8778c937853320";
+      image = "rssbridge/rss-bridge@sha256:c1ae55f116fed0706473f4d6025944a7f0cac1591478f10df86aa963865a5bde";
       autoStart = true;
       ports = [
         "8082:80"

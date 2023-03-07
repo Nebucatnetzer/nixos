15b9f96bee60fc4884afa22e9f877eb71fff02c5
Andreas Zweili <andreas@zweili.ch>
2023-03-07 20:51:58 +0100

    Configure locales

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -50,7 +50,17 @@
   };
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_NUMERIC = "de_CH.UTF-8";
+      LC_TIME = "de_CH.UTF-8";
+      LC_MONETARY = "de_CH.UTF-8";
+      LC_PAPER = "de_CH.UTF-8";
+      LC_TELEPHONE = "de_CH.UTF-8";
+      LC_MEASUREMENT = "de_CH.UTF-8";
+    };
+  };
   console = {
     font = "Lat2-Terminus16";
     keyMap = "us";

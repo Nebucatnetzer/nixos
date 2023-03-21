869109994fb58044a6a1e6bd097603ca033fdaa5
Andreas Zweili <zweili@contria.com>
2023-03-21 10:12:48 +0100

    Add ansible-language-server to eglot

--- home-manager/software/emacs/emacs.d/lib/languageserver.el
+++ home-manager/software/emacs/emacs.d/lib/languageserver.el
@@ -1,3 +1,6 @@
 (use-package eglot
   :config
+  (add-to-list 'eglot-server-programs
+               '(yaml-mode . ("ansible-language-server" "--stdio")))
+  ;; (add-hook 'yaml-mode-hook 'eglot-ensure)
   (add-hook 'python-mode-hook #'eglot-ensure))

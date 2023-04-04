65ac41b718ff0d2a057a62577fabfdddddc32b0c
Andreas Zweili <zweili@contria.com>
2023-04-04 13:48:07 +0200

    Move the environment variables to the sessionVariables

--- home-manager/common/default.nix
+++ home-manager/common/default.nix
@@ -2,31 +2,40 @@
 {
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
-  home.username = custom.username;
-  home.homeDirectory = "/home/${custom.username}";
-  home.stateVersion = custom.version;
   programs.home-manager.enable = true;
-  home.shellAliases = {
-    format-modules = "nixpkgs-fmt **/*.nix";
-    nix-generations = "sudo nix-env --list-generations --profile /nix/var/nix/profiles/system";
-    rebuild = ''
-      nixos-rebuild -j auto switch --use-remote-sudo
-    '';
-    find-garbage = "ls -l /nix/var/nix/gcroots/auto/ | sort";
-    vm = "vim";
-    less = "less -FiRX";
-    ls = "ls -lhF";
+  home = {
+    username = custom.username;
+    homeDirectory = "/home/${custom.username}";
+    stateVersion = custom.version;
+    sessionVariables = {
+      EDITOR = "vim";
+      HIGHLIGHT_STYLE = "solarized-light";
+      HISTTIMEFORMAT = "%F %T ";
+      NIXPKGS_ALLOW_UNFREE = "1";
+    };
+
+    shellAliases = {
+      format-modules = "nixpkgs-fmt **/*.nix";
+      nix-generations = "sudo nix-env --list-generations --profile /nix/var/nix/profiles/system";
+      rebuild = ''
+        nixos-rebuild -j auto switch --use-remote-sudo
+      '';
+      find-garbage = "ls -l /nix/var/nix/gcroots/auto/ | sort";
+      vm = "vim";
+      less = "less -FiRX";
+      ls = "ls -lhF";
+    };
+    packages = with pkgs; [
+      git
+      highlight
+      htop
+      killall
+      ncdu
+      nixpkgs-fmt
+      nmon
+      tree
+      unzip
+      wget
+    ];
   };
-  home.packages = with pkgs; [
-    git
-    highlight
-    htop
-    killall
-    ncdu
-    nixpkgs-fmt
-    nmon
-    tree
-    unzip
-    wget
-  ];
 }

423c5acad77a6bc530b04f79669ebb5fe39edb0f
Andreas Zweili <andreas@zweili.ch>
2023-04-08 15:09:03 +0200

    Add exercism

--- home-manager/desktop.nix
+++ home-manager/desktop.nix
@@ -29,6 +29,7 @@ in
   ];
   home.packages = with pkgs; [
     digikam
+    exercism
     freetube
     chromium
     libreoffice-fresh
--- home-manager/management.nix
+++ home-manager/management.nix
@@ -14,6 +14,7 @@ in
   ];
 
   home.packages = with pkgs; [
+    exercism
     nodePackages.prettier # formatting files
     rclone
     xclip

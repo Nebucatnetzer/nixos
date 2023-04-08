eca688ca159fa6f97ff2fc2a931a985b91b70dad
Andreas Zweili <andreas@zweili.ch>
2023-04-08 16:07:50 +0200

    Readd the router option
    I'm not quite sure if it's needed or not and currently don't have the time to
    test it.

--- systems/mobile/default.nix
+++ systems/mobile/default.nix
@@ -69,6 +69,7 @@ in
       option broadcast-address 10.213.0.255;
       option domain-name-servers 84.200.69.80, 84.200.70.40;
       subnet 10.213.0.0 netmask 255.255.255.0 {
+        option routers 10.213.0.1;
         range 10.213.0.100 10.213.0.200;
       }
     '';

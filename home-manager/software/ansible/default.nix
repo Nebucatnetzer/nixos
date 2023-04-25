747bf0e501fbea166f2ba826c89ae6198489d02d
Andreas Zweili <andreas@zweili.ch>
2023-04-25 18:40:24 +0200

    Add npm to install ansible-language-server

--- home-manager/software/ansible/default.nix
+++ home-manager/software/ansible/default.nix
@@ -1,6 +1,15 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    sshpass # it's the only system package that I need to run Ansible
-  ];
+  home = {
+    # NPM is required for the language server which I currently can only
+    # install through npm
+    sessionPath = [ "$HOME/.local/share/node_modules/bin" ];
+    sessionVariables = {
+      NPM_CONFIG_PREFIX = "$HOME/.local/share/node_modules";
+    };
+    packages = with pkgs; [
+      sshpass # it's the only system package that I need to run Ansible
+      nodejs
+    ];
+  };
 }

0678cf9213fa6770a27f23c9b0335e960d870d4d
Andreas Zweili <zweili@contria.com>
2023-04-27 09:25:27 +0200

    Extend comment

--- home-manager/software/ansible/default.nix
+++ home-manager/software/ansible/default.nix
@@ -2,7 +2,7 @@
 {
   home = {
     # NPM is required for the language server which I currently can only
-    # install through npm
+    # install through npm because the version in Nix is broken 2023-04-27
     sessionPath = [ "$HOME/.local/share/node_modules/bin" ];
     sessionVariables = {
       NPM_CONFIG_PREFIX = "$HOME/.local/share/node_modules";

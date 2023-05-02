ba4e31b9c8b9f3b6645284e6d44afb0f4e74a19f
Andreas Zweili <andreas@zweili.ch>
2023-05-02 20:51:17 +0200

    Configure company for lsp

--- home-manager/software/emacs/emacs.d/lib/languageserver.el
+++ home-manager/software/emacs/emacs.d/lib/languageserver.el
@@ -9,6 +9,8 @@
          (lsp-mode . lsp-enable-which-key-integration))
   :config
   (lsp-treemacs-sync-mode 1)
+  (setq company-minimum-prefix-length 1
+        company-idle-delay 0.0) ;; default is 0.2
   :commands lsp)
 
 ;; optionally

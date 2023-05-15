536de3509baaa85c31b507a06fb86a70fffff268
Andreas Zweili <andreas@zweili.ch>
2023-05-15 12:07:02 +0200

    Add code to overwrite the scratch-message

--- home-manager/software/emacs/emacs.d/lib/az_hyperbole.el
+++ home-manager/software/emacs/emacs.d/lib/az_hyperbole.el
@@ -1,3 +1,11 @@
 (use-package hyperbole
   :config
-  (hyperbole-mode 1))
+  (hyperbole-mode 1)
+
+  ;; https://emacs.stackexchange.com/a/55693/17595
+  (let ((file "~/nextcloud/10_documents/02_projects/projects.el"))
+    (when (file-exists-p file)
+      (setq initial-scratch-message
+            (with-temp-buffer
+              (insert-file-contents file)
+              (buffer-string))))))

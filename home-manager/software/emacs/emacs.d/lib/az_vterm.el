e5e39d897a9e2ef49e168bcd64a2cef9c1b5cc8e
Andreas Zweili <andreas@zweili.ch>
2023-05-15 10:54:23 +0200

    Remove keybindings from vterm
    It's overshadowed by hyperbole

--- home-manager/software/emacs/emacs.d/lib/az_vterm.el
+++ home-manager/software/emacs/emacs.d/lib/az_vterm.el
@@ -3,9 +3,6 @@
     :config
     (setq vterm-kill-buffer-on-exit t
           vterm-disable-bold t)
-    (global-set-key (kbd "M-RET") 'vterm)
     (evil-set-initial-state 'vterm-mode 'emacs))
 
-  (use-package multi-vterm
-    :config
-    (global-set-key (kbd "M-S-RET") 'multi-vterm)))
+  (use-package multi-vterm)

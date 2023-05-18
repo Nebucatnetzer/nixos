596ec94990bb485974a609fd0d9ae29c28213d87
Andreas Zweili <andreas@zweili.ch>
2023-05-18 23:38:20 +0200

    Adjust grobi for new monitor

--- home-manager/software/grobi/default.nix
+++ home-manager/software/grobi/default.nix
@@ -5,22 +5,10 @@
     rules = [
       {
         name = "docked";
-        outputs_connected = [ "DP-1-1" "DP-1-2" ];
+        outputs_connected = [ "eDP-1" "DP-1-1" ];
         atomic = true;
-        configure_row = [ "DP-1-2" "DP-1-1" ];
+        configure_row = [ "DP-1-1" "eDP-1" ];
         primary = "DP-1-1";
-        execute_after = [
-          "${pkgs.nitrogen}/bin/nitrogen --restore"
-          "${pkgs.qtile}/bin/qtile cmd-obj -o cmd -f restart"
-        ];
-      }
-      {
-        name = "gaming";
-        outputs_connected = [ "DP-1-2" ];
-        outputs_disconnected = [ "DP-1-1" ];
-        configure_single = "DP-1-2";
-        primary = true;
-        atomic = true;
         execute_after = [
           "${pkgs.nitrogen}/bin/nitrogen --restore"
           "${pkgs.qtile}/bin/qtile cmd-obj -o cmd -f restart"

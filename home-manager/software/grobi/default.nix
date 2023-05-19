0ed806ba97ce9296e5c9c2f3e141560f3145150e
Andreas Zweili <andreas@zweili.ch>
2023-05-19 23:36:56 +0200

    Reconfigure grobi

--- home-manager/software/grobi/default.nix
+++ home-manager/software/grobi/default.nix
@@ -5,18 +5,19 @@
     rules = [
       {
         name = "docked";
-        outputs_connected = [ "eDP-1" "DP-1-1" ];
+        outputs_connected = [ "eDP-1" "DP-1" ];
         atomic = true;
-        configure_row = [ "DP-1-1" "eDP-1" ];
+        configure_row = [ "DP-1" "eDP-1" ];
         primary = "DP-1-1";
         execute_after = [
           "${pkgs.nitrogen}/bin/nitrogen --restore"
           "${pkgs.qtile}/bin/qtile cmd-obj -o cmd -f restart"
+          "${pkgs.networkmanager}/bin/nmcli radio wifi off"
         ];
       }
       {
         name = "undocked";
-        outputs_disconnected = [ "DP-1-1" "DP-1-2" ];
+        outputs_disconnected = [ "DP-1" ];
         configure_single = "eDP-1";
         primary = true;
         atomic = true;

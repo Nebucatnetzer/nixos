e5fa7e159f7aa5ba0bef13ce233834e4da0059a5
Andreas Zweili <andreas@zweili.ch>
2023-05-25 22:05:16 +0200

    Unset the keybindings from python mode

--- home-manager/software/emacs/emacs.d/lib/az_python.el
+++ home-manager/software/emacs/emacs.d/lib/az_python.el
@@ -3,5 +3,7 @@
   (setq python-shell-interpreter "python3"))
 
 (use-package python-pytest
+  :config
+  (unbind-key "C-c C-t" python-mode-map)
   :bind
   (:map python-mode-map ("C-c C-t" . python-pytest-function-dwim)))

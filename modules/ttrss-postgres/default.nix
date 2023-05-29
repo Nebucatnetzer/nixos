91191f79b29510d7ef7769fc0e427fbf92aebb90
Andreas Zweili <andreas@zweili.ch>
2023-05-29 16:21:59 +0200

    Remove unused code

--- modules/ttrss-postgres/default.nix
+++ modules/ttrss-postgres/default.nix
@@ -12,8 +12,6 @@ let
   # https://github.com/Nebucatnetzer/tt-rss-aarch64/pkgs/container/tt-rss-aarch64%2Fttrss-fpm-pgsql-static/versions
   ttrssImage = "ghcr.io/nebucatnetzer/tt-rss-aarch64/ttrss-fpm-pgsql-static@sha256:4842ca145ad3d57b1b627fdf9ea4349aeeda9e31134deee1e6a64694f6825754";
   ttrssService = "${config.virtualisation.oci-containers.backend}-ttrss";
-  backupService = "${config.virtualisation.oci-containers.backend}-backup";
-  updaterService = "${config.virtualisation.oci-containers.backend}-updater";
 in
 {
   imports = [
--- systems/raspi4/init_config.nix
+++ systems/raspi4/init_config.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 {
   imports = [ "${fetchTarball "https://github.com/NixOS/nixos-hardware/archive/32f61571b486efc987baca553fb35df22532ba63.tar.gz" }/raspberry-pi/4" ];
 

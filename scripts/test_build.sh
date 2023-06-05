f3ef539c9dd208a685cd52c89289a880869163ed
Andreas Zweili <andreas@zweili.ch>
2023-06-05 10:09:24 +0200

    Add a skip list to test_build

--- scripts/test_build.sh
+++ scripts/test_build.sh
@@ -2,9 +2,20 @@
 set -e
 
 hosts=($(echo `nix eval .#nixosConfigurations --apply 'pkgs: builtins.concatStringsSep " " (builtins.attrNames pkgs)'` | xargs ))
+skip=(
+    "desktop-vm"
+    "gwyn"
+    "loki-test"
+    "test-raspi"
+    "staubfinger"
+)
+
 
 for host in "${hosts[@]}"
 do
+    if [[ " ${skip[*]} " =~ " ${host} " ]];then
+        continue
+    fi
     echo $host
     nixos-rebuild dry-build --flake .#${host}
     echo

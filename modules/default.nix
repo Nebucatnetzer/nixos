d0da0651f8d6ca10bcbb64471cac7fe847b0ad8f
Andreas Zweili <andreas@zweili.ch>
2023-06-05 15:08:13 +0200

    Move gitea to modules/services

--- modules/default.nix
+++ modules/default.nix
@@ -20,7 +20,7 @@
     ./services/data-share
     ./services/docker
     ./services/docker-mailserver
-    ./gitea
+    ./services/gitea
     ./services/logs-share
     ./services/log-to-ram
     ./services/pipewire

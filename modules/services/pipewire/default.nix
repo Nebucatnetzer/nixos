41a53c390dffaf249a6c2ebe77e68cb46e9f18f0
Andreas Zweili <andreas@zweili.ch>
2023-06-16 17:29:01 +0200

    Remove rtkit
    I don't think I need it and it spams my logs

--- modules/services/pipewire/default.nix
+++ modules/services/pipewire/default.nix
@@ -9,7 +9,6 @@ in
 
   config = lib.mkIf cfg.enable {
     hardware.pulseaudio.enable = lib.mkForce false;
-    security.rtkit.enable = true;
     services.pipewire = {
       enable = true;
       alsa.enable = true;

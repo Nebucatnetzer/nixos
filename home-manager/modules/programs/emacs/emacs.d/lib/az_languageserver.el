636c6d6350dfe0894186c250093d0697c631ae75
Andreas Zweili <andreas@zweili.ch>
2023-06-21 21:14:32 +0200

    Add some performance improvements for language server

--- home-manager/modules/programs/emacs/emacs.d/lib/az_languageserver.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_languageserver.el
@@ -10,6 +10,9 @@
   (lsp-treemacs-sync-mode 1)
   (setq company-minimum-prefix-length 1
         company-idle-delay 0.0 ;; default is 0.2
+        gc-cons-threshold 100000000
+        read-process-output-max (* 1024 1024)
+        lsp-idle-delay 0.500
         lsp-pylsp-plugins-pylint-enabled t)
   :commands lsp)
 

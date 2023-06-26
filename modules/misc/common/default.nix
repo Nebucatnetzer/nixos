695a0043e2f598c500ae5a41b1c6bf4749555d52
Andreas Zweili <andreas@zweili.ch>
2023-06-26 14:05:47 +0200

    Deploy without sudo password

--- modules/misc/common/default.nix
+++ modules/misc/common/default.nix
@@ -113,17 +113,31 @@
   };
 
   security.sudo = {
-    extraRules = [
-      {
-        users = [ "%wheel" ];
-        commands = [
-          {
-            command = "ALL";
-            options = [ "SETENV" "NOPASSWD" ];
-          }
-        ];
-      }
-    ];
+    extraRules =
+      let
+        storePrefix = "/nix/store/*";
+        systemName = "nixos-system-${config.networking.hostName}-*";
+      in
+      [
+        {
+          commands = [
+            {
+              command = "${storePrefix}-nix-*/bin/nix-env -p /nix/var/nix/profiles/system --set ${storePrefix}-${systemName}";
+              options = [ "NOPASSWD" ];
+            }
+          ];
+          groups = [ "wheel" ];
+        }
+        {
+          commands = [
+            {
+              command = "${storePrefix}-${systemName}/bin/switch-to-configuration";
+              options = [ "NOPASSWD" ];
+            }
+          ];
+          groups = [ "wheel" ];
+        }
+      ];
   };
 
 

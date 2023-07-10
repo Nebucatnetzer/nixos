d1181f27f2f04fcd82136b3ce937f94887833d19
Andreas Zweili <andreas@zweili.ch>
2023-07-10 09:31:20 +0200

    Update gitea

--- modules/services/gitea/default.nix
+++ modules/services/gitea/default.nix
@@ -24,7 +24,7 @@ in
       containers."gitea" = {
         # https://blog.gitea.io/
         # https://hub.docker.com/r/gitea/gitea/tags
-        image = "gitea/gitea:1.19.3";
+        image = "gitea/gitea:1.19.4";
         autoStart = true;
         environment = {
           PUID = "1000";

d25b92d29b5c48c768188fe9a2b5e223bb0a98dd
Andreas Zweili <zweili@contria.com>
2023-07-21 08:57:27 +0200

    Allow the work host to access the VM

--- systems/desktop-vm/default.nix
+++ systems/desktop-vm/default.nix
@@ -30,6 +30,11 @@
     { device = "/dev/disk/by-label/swap"; }
   ];
 
+  users.users.${config.az-username} = {
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCcwq2DNEJyum5Q362dlC97MW9IBJSiL4ZqXmQxdXSCiKvad6QSVic2UE+iBSzpOigB0uqGPH5iCQ2tgnTVB0n3Ttv0FV9Nchu8hEJA6tmdMOvAifVptv4KWU0H7uUI0EV1UewUznofvw67EbbQYhZiT0ydHgjohBBuvrhptME4YBaU74+ZeIDuVfQBRcj2dfvfQKZuJzyRcNg9Ms5XKMkJ2y2Z0KPX9dX91mAHio0grBtck+G2+OlswRmhE67lXoOkeiIqPo158QZEWEakfMCxHBP7KAE6OKevzXcViDksTzVy21LGGFaPvUMDszVal6Gq4WVK0yRjvIqzz66v8QVpsJ7mlsUM3vPbWphNjRNMq7UHcHyI8SYEKtzwC6CFCySMJLZNYPHGP6Q+zsqeSsD9DlxK0i/d5Yzs8YRImqcbdkFc56xJ5s4hd9mP4t4jIuiDR3hXQCIgoO2+a2R0nrRrmw4vD05hnye5+BYFnYLtsvgJGnEhKlmmBpL/T/kcUWk= andreas@co-ws-con4"
+    ];
+  };
   profiles.az-desktop.enable = true;
   services = {
     az-logs-share.enable = true;

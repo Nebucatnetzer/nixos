28fc9fcbf2a457cceeb1b125041790dc2597455e
Andreas Zweili <andreas@zweili.ch>
2023-07-22 14:23:15 +0200

    Add littler helper script

--- home-manager/profiles/desktop.nix
+++ home-manager/profiles/desktop.nix
@@ -1,4 +1,14 @@
 { inputs, pkgs, ... }:
+let
+  az-media = pkgs.writeShellScriptBin "az-media" ''
+    videos="videos"
+    directory="''${1:-videos}"
+    for i in $(seq 1 4);
+    do
+        nvidia-offload mpv --shuffle "/run/media/andreas/e7c05e7e-79ec-45af-b1c8-840020fcf708/$directory/" &
+    done
+  '';
+in
 {
   imports = [
     "${inputs.self}/home-manager/modules"
@@ -13,6 +23,7 @@
       shotwell
       sound-juicer
       unstable.tagger
+      az-media
     ];
   };
 

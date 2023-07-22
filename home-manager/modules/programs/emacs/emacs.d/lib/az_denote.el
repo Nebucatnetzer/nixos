a374a792ecb04ea08192530551f09f5041575ddd
Andreas Zweili <andreas@zweili.ch>
2023-07-22 14:23:22 +0200

    Change keybindings for denote

--- home-manager/modules/programs/emacs/emacs.d/lib/az_denote.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_denote.el
@@ -1,7 +1,7 @@
 (use-package denote
   :bind
-  (("C-c n r" . denote-rename-file)
-   ("C-c n n" . denote-create-file))
+  (("C-c C-n r" . denote-rename-file)
+   ("C-c C-n n" . denote-create-file))
   :config
   (setq denote-file-type "markdown-yaml"
         denote-directory "~/nextcloud/10_documents/"))

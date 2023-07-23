6a9ae427a9e66c8b99fd4f307d3c648bab89fa07
Andreas Zweili <andreas@zweili.ch>
2023-07-23 10:34:27 +0200

    Mute MPV

--- home-manager/profiles/desktop.nix
+++ home-manager/profiles/desktop.nix
@@ -5,7 +5,7 @@ let
     directory="''${1:-videos}"
     for i in $(seq 1 4);
     do
-        nvidia-offload mpv --shuffle "/run/media/andreas/e7c05e7e-79ec-45af-b1c8-840020fcf708/$directory/" &
+        nvidia-offload mpv --shuffle --mute=yes "/run/media/andreas/e7c05e7e-79ec-45af-b1c8-840020fcf708/$directory/" &
     done
   '';
 in

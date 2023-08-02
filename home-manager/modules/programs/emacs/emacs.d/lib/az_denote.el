352e9c15583db0a2e5eb18b0f15a32cb31e8ab79
Andreas Zweili <zweili@contria.com>
2023-08-02 08:45:57 +0200

    Changes related to denote

--- home-manager/modules/programs/emacs/emacs.d/lib/az_denote.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_denote.el
@@ -1,7 +1,8 @@
 (use-package denote
   :bind
-  (("C-c n r" . denote-rename-file)
-   ("C-c n n" . denote-create-file))
+  (("C-c C-n r" . denote-rename-file)
+   ("C-c C-n n" . denote-create-file))
   :config
   (setq denote-file-type "markdown-yaml"
-        denote-directory "~/nextcloud/10_documents/"))
+        denote-directory "~/nextcloud/10_documents/"
+        denote-excluded-directories-regexp "99_archive/*"))

898bbf1fabb22ab7feee48eccc36438bf9402255
Andreas Zweili <andreas@zweili.ch>
2023-08-07 10:19:33 +0200

    Update tt-rss

--- modules/services/ttrss-postgres/default.nix
+++ modules/services/ttrss-postgres/default.nix
@@ -11,7 +11,7 @@ let
     TTRSS_PLUGINS = "af_comics, af_readability, auth_internal, hotkeys_swap_jk, nginx_xaccel";
   };
   # https://github.com/Nebucatnetzer/tt-rss-aarch64/pkgs/container/tt-rss-aarch64%2Fttrss-fpm-pgsql-static/versions
-  ttrssImage = "ghcr.io/nebucatnetzer/tt-rss-aarch64/ttrss-fpm-pgsql-static@sha256:a7cb5c6291f7c2ecb2fe28a61c263a1a563ed8f759a7c6c4c33d2fcbe8410ff0";
+  ttrssImage = "ghcr.io/nebucatnetzer/tt-rss-aarch64/ttrss-fpm-pgsql-static@sha256:74163d83e265dbe47e28c17a688e147dc3907248c2e36596aeb8dcbe4fdfd8fa";
   ttrssService = "${config.virtualisation.oci-containers.backend}-ttrss";
 in
 {

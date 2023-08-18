ee46337409f0ef2e3336fee1ec863159a9f792bf
Andreas Zweili <zweili@contria.com>
2023-08-18 14:10:02 +0200

    Add ~/.local/bin to PATH

--- home-manager/profiles/work-wsl.nix
+++ home-manager/profiles/work-wsl.nix
@@ -16,6 +16,7 @@
     sessionPath = [ "$HOME/node_modules/.bin" ];
     sessionVariables = {
       NIX_PATH = "nixpkgs=${inputs.nixpkgs}";
+      PATH = "PATH:~/.local/bin";
     };
     packages = with pkgs; [
       keychain

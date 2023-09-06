e5805304c083f606a4b98dad9a27ef7f20d9adc7
Andreas Zweili <zweili@contria.com>
2023-09-06 12:56:29 +0200

    Disable line-numbers in vterm

--- home-manager/modules/programs/emacs/emacs.d/lib/az_vterm.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_vterm.el
@@ -6,4 +6,5 @@
   (add-to-list 'vterm-tramp-shells '("sshx" "/usr/bin/env bash") '("ssh" "/usr/bin/env bash"))
   (setq vterm-kill-buffer-on-exit t
         vterm-disable-bold t)
-  (evil-set-initial-state 'vterm-mode 'emacs))
+  (evil-set-initial-state 'vterm-mode 'emacs)
+  (add-hook 'vterm-mode-hook (lambda() (display-line-numbers-mode -1))))

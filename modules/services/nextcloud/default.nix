678379396ef458b810430221b05132694da737d9
Andreas Zweili <andreas@zweili.ch>
2023-09-18 17:51:04 +0200

    Update Nextcloud

--- modules/services/nextcloud/default.nix
+++ modules/services/nextcloud/default.nix
@@ -13,7 +13,7 @@ let
   };
   networkName = "nextcloud";
   # https://github.com/Nebucatnetzer/nextcloud-smb
-  nextcloudImage = "ghcr.io/nebucatnetzer/nextcloud-smb/nextcloud-smb:27.0.2@sha256:9c1c301fca6729bbd3270865b1dd63efd7b8baca1964d9324cabc24d7867d97b";
+  nextcloudImage = "ghcr.io/nebucatnetzer/nextcloud-smb/nextcloud-smb:27.0.2@sha256:6f6e8a98ea0d10ce5cefe6315158461d8392d8efbc2ca63ce0af2c40e741a36c";
   nextcloudService = "${config.virtualisation.oci-containers.backend}-nextcloud";
   cronService = "${config.virtualisation.oci-containers.backend}-cron";
 in

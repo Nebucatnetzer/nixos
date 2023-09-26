5587b7b3b95e017e84df2a42fcf980789729d942
Andreas Zweili <andreas@zweili.ch>
2023-09-26 20:57:03 +0200

    Change vterm-shell to bash

--- home-manager/modules/programs/emacs/emacs.d/lib/az_vterm.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_vterm.el
@@ -6,7 +6,7 @@
   (add-to-list 'vterm-tramp-shells '("ssh" "/usr/bin/env bash"))
   (add-to-list 'vterm-tramp-shells '("sshx" "/usr/bin/env bash"))
   (setq vterm-kill-buffer-on-exit t
-        vterm-shell "/usr/bin/env bash"
+        vterm-shell "bash"
         vterm-disable-bold t)
   (evil-set-initial-state 'vterm-mode 'emacs)
   (add-hook 'vterm-mode-hook (lambda() (display-line-numbers-mode -1))))

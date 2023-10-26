69f7511f0f807b2accdf2f2804ca1f705ba1084d
Andreas Zweili <zweili@contria.com>
2023-10-26 09:37:38 +0200

    Replace the custom scratch buffer with a personal hyberbole file

--- home-manager/modules/programs/emacs/emacs.d/lib/az_hyperbole.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_hyperbole.el
@@ -1,11 +1,4 @@
 (use-package hyperbole
   :config
-  (hyperbole-mode 1)
-
-  ;; https://emacs.stackexchange.com/a/55693/17595
-  (let ((file "~/nextcloud/10_documents/02_projects/projects.el"))
-    (when (file-exists-p file)
-      (setq initial-scratch-message
-            (with-temp-buffer
-              (insert-file-contents file)
-              (buffer-string))))))
+  (setq hbmap:dir-user "~/nextcloud/10_documents/99_archive/0000/hyperbole/")
+  (hyperbole-mode 1))

53e7f4a74e1f81fee5201b61df14b4a7799ea27f
Andreas Zweili <andreas@zweili.ch>
2023-11-08 17:34:41 +0100

    Enable thumbnails for video files

--- modules/programs/nautilus/default.nix
+++ modules/programs/nautilus/default.nix
@@ -10,6 +10,7 @@ in
   config = lib.mkIf cfg.enable {
     environment = {
       systemPackages = with pkgs; [
+        ffmpegthumbnailer
         gnome.nautilus
         libheif.bin
         libheif.out

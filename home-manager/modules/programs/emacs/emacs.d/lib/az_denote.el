f572a02d7e787f49bb95124439e973c4390fe269
Andreas Zweili <zweili@contria.com>
2023-11-17 08:46:03 +0100

    Shorten name of note function

--- home-manager/modules/programs/emacs/emacs.d/lib/az_denote.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_denote.el
@@ -2,10 +2,10 @@
   (use-package denote
     :bind
     (("C-c n r" . denote-rename-file)
-     ("C-c n p" . az-denote-create-new-note-from-region)
+     ("C-c n p" . az-note-from-region)
      ("C-c n n" . denote))
     :config
-    (defun az-denote-create-new-note-from-region (beg end)
+    (defun az-note-from-region (beg end)
       "Create note whose contents include the text between BEG and END. Prompt
     for title and keywords of the new note."
       (interactive "r")

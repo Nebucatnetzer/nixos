96bc5e4fac410e70a032632ef45abbe66c36f19d
Andreas Zweili <zweili@contria.com>
2023-11-29 09:49:41 +0100

    Add alias for home-manager switch

--- home-manager/profiles/work-wsl.nix
+++ home-manager/profiles/work-wsl.nix
@@ -59,6 +59,7 @@
       shellAliases = {
         work-management = "mosh --ssh='ssh -i ~/.ssh/zweili.key' zweili@10.49.0.100 -- tmux new -A -s 0";
         work-vm = ''ssh andreas@localhost -p 2222 -t "$@" "tmux new -A -s 0"'';
+        hm-rebuild = "home-manager switch |& nom";
       };
     };
   };

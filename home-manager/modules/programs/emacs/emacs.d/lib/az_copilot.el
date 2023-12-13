4227c172d5f811161f5f32536dea9d0bb9e4284b
Andreas Zweili <zweili@contria.com>
2023-12-13 18:21:07 +0100

    Run copilot after company

--- home-manager/modules/programs/emacs/emacs.d/lib/az_copilot.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_copilot.el
@@ -12,5 +12,6 @@
   :config
   (add-hook 'prog-mode-hook 'copilot-mode)
   (add-hook 'yaml-mode-hook 'copilot-mode)
+  (add-to-list 'copilot-disable-display-predicates #'company--active-p)
   (define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
   (define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion))

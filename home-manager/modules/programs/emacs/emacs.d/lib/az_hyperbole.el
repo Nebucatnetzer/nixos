9b7b014949f871c66dd09a73b794d2a41c5395e5
Andreas Zweili <zweili@contria.com>
2023-12-14 12:51:22 +0100

    Disable keybinding for hui-search-web

--- home-manager/modules/programs/emacs/emacs.d/lib/az_hyperbole.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_hyperbole.el
@@ -1,4 +1,5 @@
 (use-package hyperbole
   :config
   (setq hbmap:dir-user "~/nextcloud/10_documents/99_archive/0000/hyperbole/")
+  (define-key hyperbole-mode-map (kbd "C-c /") nil)
   (hyperbole-mode 1))

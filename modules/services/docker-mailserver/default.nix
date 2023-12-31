95a256b81b4a9583e699356ad871fc1c5bc05b23
Andreas Zweili <andreas@zweili.ch>
2023-12-31 14:30:11 +0100

    Update docker-mailserver

--- modules/services/docker-mailserver/default.nix
+++ modules/services/docker-mailserver/default.nix
@@ -1,7 +1,7 @@
 { config, inputs, lib, pkgs, ... }:
 let
   cfg = config.services.az-mailserver;
-  version = "13.0.1";
+  version = "13.1.0";
   mailserver-setup = (pkgs.writeScriptBin "mailserver-setup"
     "${builtins.readFile (pkgs.fetchurl {
       url = "https://raw.githubusercontent.com/docker-mailserver/docker-mailserver/v${version}/setup.sh";

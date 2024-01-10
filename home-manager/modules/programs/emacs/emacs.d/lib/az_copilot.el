f5476ac18052240b3c747156ee7c0b505bb865a9
Andreas Zweili <zweili@contria.com>
2024-01-10 08:49:08 +0100

    Add a hook for copilot

--- home-manager/modules/programs/emacs/emacs.d/lib/az_copilot.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_copilot.el
@@ -12,6 +12,10 @@
   :config
   (add-hook 'prog-mode-hook 'copilot-mode)
   (add-hook 'yaml-mode-hook 'copilot-mode)
+
+  ;; write a hook that runs copilot-clear-overlay after company is finished.
+  (add-hook 'company-completion-finished-hook 'copilot-clear-overlay)
+
   (add-to-list 'copilot-disable-display-predicates #'company--active-p)
   (define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
   (define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion))

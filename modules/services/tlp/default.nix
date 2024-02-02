19c4bcbd69b83efe1962865191e0106214f90981
Andreas Zweili <andreas@zweili.ch>
2024-02-02 16:40:20 +0100

    Enable bluetooth at startup

--- modules/services/tlp/default.nix
+++ modules/services/tlp/default.nix
@@ -11,7 +11,7 @@ in
     services.tlp = {
       enable = true;
       settings = {
-        DEVICES_TO_DISABLE_ON_STARTUP = "bluetooth wifi wwan";
+        DEVICES_TO_DISABLE_ON_STARTUP = "wifi wwan";
         DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE = "bluetooth wifi wwan";
       };
     };

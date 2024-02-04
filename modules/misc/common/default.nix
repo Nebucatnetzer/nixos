4a898e91d7d29b1ba0010874a9be69a457f8effa
Andreas Zweili <andreas@zweili.ch>
2024-02-04 13:58:16 +0100

    Update state version to 23.11

--- modules/misc/common/default.nix
+++ modules/misc/common/default.nix
@@ -156,5 +156,5 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05";
+  system.stateVersion = "23.11";
 }

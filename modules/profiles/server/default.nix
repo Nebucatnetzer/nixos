29c65f35ae21d77218ff34826e6cebc20381701e
Andreas Zweili <andreas@zweili.ch>
2024-02-20 07:29:39 +0100

    Explicitly define which system to use when updating

--- modules/profiles/server/default.nix
+++ modules/profiles/server/default.nix
@@ -16,7 +16,7 @@ in
     system.autoUpgrade = {
       enable = true;
       dates = "03:00";
-      flake = "git+https://git.2li.ch/Nebucatnetzer/nixos";
+      flake = "git+https://git.2li.ch/Nebucatnetzer/nixos#${config.networking.hostName}";
     };
   };
 }

eda8fa9c61b87e37fc63841ab196ff38ba698623
Andreas Zweili <zweili@contria.com>
2024-02-23 09:36:17 +0100

    Remove unused package lsp-nix

--- home-manager/modules/programs/emacs/emacs.d/lib/az_nix.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_nix.el
@@ -1,9 +1,2 @@
-(use-package lsp-nix
-  :after (lsp-mode)
-  :demand t
-  :ensure nil
-  :custom
-  (lsp-nix-nil-formatter ["nixpkgs-fmt"]))
-
 (use-package nix-mode
   :hook (nix-mode . lsp-deferred))

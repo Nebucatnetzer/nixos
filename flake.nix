462c4899dd2e3984a939d6dc1b922f41e4796128
Andreas Zweili <andreas@zweili.ch>
2024-04-08 21:39:43 +0200

    Remove empty lines

--- flake.nix
+++ flake.nix
@@ -1,18 +1,14 @@
 {
   description = "Andreas Zweili's Nixos configuration";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-
     # look here for the hardware options https://github.com/NixOS/nixos-hardware/blob/master/flake.nix#L5
     nixos-hardware.url = "github:nixos/nixos-hardware";
-
     agenix = {
       url = "github:ryantm/agenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";

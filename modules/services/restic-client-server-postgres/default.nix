63d6bb90e7c448136861d207704a713023632b6c
Andreas Zweili <andreas@zweili.ch>
2024-04-16 19:22:53 +0200

    Update postgres version

--- modules/services/restic-client-server-postgres/default.nix
+++ modules/services/restic-client-server-postgres/default.nix
@@ -56,7 +56,7 @@ in
           --exclude-file=${inputs.self}/modules/misc/restic-client/excludes.txt \
           --tag ${cfg.tag} ${cfg.path}
 
-        ${pkgs.sudo}/bin/sudo -u postgres ${pkgs.postgresql_14}/bin/pg_dumpall | \
+        ${pkgs.sudo}/bin/sudo -u postgres ${pkgs.postgresql_15}/bin/pg_dumpall | \
         ${pkgs.restic}/bin/restic backup \
           --tag postgres \
           --stdin \

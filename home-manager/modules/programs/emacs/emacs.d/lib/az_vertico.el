7b433a4dab1adfc1e7bf9e7c947228da45956ea7
Andreas Zweili <andreas@zweili.ch>
2024-05-14 21:13:07 +0200

    Add options related to completion and vertico

--- home-manager/modules/programs/emacs/emacs.d/lib/az_vertico.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_vertico.el
@@ -8,7 +8,21 @@
   :init
   (savehist-mode))
 
-;; Emacs 28 and newer: Hide commands in M-x which do not work in the current
-;; mode.  Vertico commands are hidden in normal buffers. This setting is
-;; useful beyond Vertico.
-(setq read-extended-command-predicate #'command-completion-default-include-p)
+;; A few more useful configurations...
+(use-package emacs
+  :init
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+  ;; Support opening new minibuffers from inside existing minibuffers.
+  (setq enable-recursive-minibuffers t)
+
+  (setq read-file-name-completion-ignore-case t
+        read-buffer-completion-ignore-case t)
+
+  ;; Emacs 28 and newer: Hide commands in M-x which do not work in the current
+  ;; mode.  Vertico commands are hidden in normal buffers. This setting is
+  ;; useful beyond Vertico.
+  (setq read-extended-command-predicate #'command-completion-default-include-p))

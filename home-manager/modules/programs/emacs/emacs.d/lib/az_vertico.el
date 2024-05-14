7839a5567916f9b424aa2ae0340232231095f589
Andreas Zweili <andreas@zweili.ch>
2024-05-14 21:18:14 +0200

    Enable vertico directory
    Allows to expand and delete directories faster.

--- home-manager/modules/programs/emacs/emacs.d/lib/az_vertico.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_vertico.el
@@ -8,6 +8,18 @@
   :init
   (savehist-mode))
 
+;; Configure directory extension.
+(use-package vertico-directory
+  :after vertico
+  :ensure nil
+  ;; More convenient directory navigation commands
+  :bind (:map vertico-map
+              ("RET" . vertico-directory-enter)
+              ("DEL" . vertico-directory-delete-char)
+              ("M-DEL" . vertico-directory-delete-word))
+  ;; Tidy shadowed file names
+  :hook (rfn-eshadow-update-overlay . vertico-directory-tidy))
+
 ;; A few more useful configurations...
 (use-package emacs
   :init

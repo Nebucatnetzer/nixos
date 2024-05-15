64a29ed061ea0772c1c24291daf8cd14dcb3e475
Andreas Zweili <zweili@contria.com>
2024-05-15 09:43:07 +0200

    Remove duplicate code

--- home-manager/modules/programs/emacs/emacs.d/lib/az_projectile.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_projectile.el
@@ -9,6 +9,5 @@
         ;;projectile-completion-system 'ivy
         projectile-git-fd-args "-H -0 -E .git -tf --strip-cwd-prefix -c never"
         projectile-switch-project-action #'projectile-dired)
-  (setq projectile-switch-project-action #'projectile-dired)
   :bind (:map projectile-mode-map
               ("C-x p" . projectile-command-map)))

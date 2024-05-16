d166a3222e54fa008b02e612adf5275aafa6fdfa
Andreas Zweili <zweili@contria.com>
2024-05-16 11:48:36 +0200

    Remove unused lines

--- home-manager/modules/programs/emacs/emacs.d/lib/az_embark.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_embark.el
@@ -1,15 +1,12 @@
 ;; -*- lexical-binding: t; -*-
 ;; https://github.com/oantolin/embark
 (use-package embark
-  :ensure t
-
   :bind
   (("C-." . embark-act)         ;; pick some comfortable binding
    ("C-;" . embark-dwim)        ;; good alternative: M-.
    ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
 
   :init
-
   ;; Optionally replace the key help with a completing-read interface
   (setq prefix-help-command #'embark-prefix-help-command)
 
@@ -23,7 +20,6 @@
   ;; (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
 
   :config
-
   ;; Hide the mode line of the Embark live/completions buffers
   (add-to-list 'display-buffer-alist
                '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
@@ -32,6 +28,5 @@
 
 ;; Consult users will also want the embark-consult package.
 (use-package embark-consult
-  :ensure t ; only need to install it, embark loads it after consult if found
   :hook
   (embark-collect-mode . consult-preview-at-point-mode))

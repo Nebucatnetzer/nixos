275d00a808682d64cf485bcc68d12d01e2c0ac00
Andreas Zweili <andreas@zweili.ch>
2024-05-20 15:23:20 +0200

    Allow files with spaces

--- home-manager/modules/programs/emacs/emacs.d/lib/az_org.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_org.el
@@ -190,6 +190,8 @@
 (defun az-markdown-convert-buffer-to-org ()
   "Convert the current buffer's content from markdown to orgmode format and save it with the current buffer's file name but with .org extension."
   (interactive)
-  (shell-command-on-region (point-min) (point-max)
-                           (format "pandoc -f markdown -t org -o %s"
-                                   (concat (file-name-sans-extension (buffer-file-name)) ".org"))))
+  (let ((convert-command (format "pandoc -f markdown -t org -o %s"
+                                 (concat (shell-quote-argument(file-name-sans-extension (buffer-file-name))) ".org"))))
+    (message convert-command)
+    (shell-command-on-region (point-min) (point-max)
+                             convert-command)))

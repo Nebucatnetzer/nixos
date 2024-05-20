ffe068eb76792428a7b04c0c14c1e784c9f58624
Andreas Zweili <andreas@zweili.ch>
2024-05-20 16:15:30 +0200

    Remove old hydra bodies

--- home-manager/modules/programs/emacs/emacs.d/lib/az_hydra.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_hydra.el
@@ -1,52 +1,6 @@
 ;; -*- lexical-binding: t; -*-
 (use-package hydra)
 
-(defhydra hydra-buffer (:color red :columns 3)
-  "
-                Buffers :
-  "
-  ("n" next-buffer "next")
-  ("b" consult-buffer "switch")
-  ("B" ibuffer "ibuffer")
-  ("p" previous-buffer "prev")
-  ("C-b" buffer-menu "buffer menu")
-  ("N" evil-buffer-new "new")
-  ("d" kill-this-buffer "delete")
-  ;; don't come back to previous buffer after delete
-  ("D" (progn (kill-this-buffer) (next-buffer)) "Delete")
-  ("s" save-buffer "save"))
-
-(defhydra hydra-window-operations (:color red :columns 3)
-  "
-                    Windows :
-"
-  ("h" windmove-left "left")
-  ("l" windmove-right "right")
-  ("j" windmove-down "down")
-  ("k" windmove-up "up")
-  ("w" (lambda () (interactive) (other-window 1)) "Unkown")
-  ("h" split-window-vertically "horizontal split")
-  ("v" split-window-horizontally "vertical split")
-  ("o" delete-other-windows "Delete all other")
-  ("X" delete-window "Delete this")
-  ("=" balance-windows "Balance")
-  ("K" kill-this-buffer "Kill Buffer")
-  ("D" kill-all-dired-buffers "Kill all dired")
-  ("S" toggle-window-split "Toggle Split"))
-
-(defhydra hydra-ediff (:color blue :columns 3)
-  "
-                    Ediff :
-"
-  ("b" ediff-buffers "buffers")
-  ("B" ediff-buffers3 "buffers 3-way")
-  ("f" ediff-files "files")
-  ("F" ediff-files3 "files 3-way")
-  ("c" ediff-current-file "current file")
-  ("r" ediff-revision "revision")
-  ("l" ediff-regions-linewise "linewise")
-  ("w" ediff-regions-wordwise "wordwise"))
-
 (defhydra hydra-yasnippet (:color blue :columns 3)
   "
                     Yasnippet:
@@ -152,60 +106,7 @@
   ("s" ispell "Correct Spelling")
   ("d" ispell-change-dictionary "Change dictionary"))
 
-                                        ;(define-key dired-mode-map (kbd "'") #'hydra-dired/body)
-                                        ;https://github.com/abo-abo/hydra/wiki/Dired
-(defhydra hydra-dired (:hint nil :color pink)
-  "
-_+_ mkdir          _v_iew           _m_ark             _(_ details        _i_nsert-subdir    wdired
-_C_opy             _O_ view other   _U_nmark all       _)_ omit-mode      _$_ hide-subdir    C-x C-q : edit
-_D_elete           _o_pen other     _u_nmark           _l_ redisplay      _w_ kill-subdir    C-c C-c : commit
-_R_ename           _M_ chmod        _t_oggle           _g_ revert buf     _e_ ediff          C-c ESC : abort
-_Y_ rel symlink    _G_ chgrp        _E_xtension mark   _s_ort             _=_ pdiff
-_S_ymlink          ^ ^              _F_ind marked      _._ toggle hydra   \\ flyspell
-_r_sync            ^ ^              ^ ^                ^ ^                _?_ summary
-_z_ compress-file  _A_ find regexp
-_Z_ compress       _Q_ repl regexp
-
-T - tag prefix
-"
-  ("\\" dired-do-ispell)
-  ("(" dired-hide-details-mode)
-  (")" dired-omit-mode)
-  ("+" dired-create-directory)
-  ("=" diredp-ediff)         ;; smart diff
-  ("?" dired-summary)
-  ("$" diredp-hide-subdir-nomove)
-  ("A" dired-do-find-regexp)
-  ("C" dired-do-copy)        ;; Copy all marked files
-  ("D" dired-do-delete)
-  ("E" dired-mark-extension)
-  ("e" dired-ediff-files)
-  ("F" dired-do-find-marked-files)
-  ("G" dired-do-chgrp)
-  ("g" revert-buffer)        ;; read all directories again (refresh)
-  ("i" dired-maybe-insert-subdir)
-  ("l" dired-do-redisplay)   ;; relist the marked or singel directory
-  ("M" dired-do-chmod)
-  ("m" dired-mark)
-  ("O" dired-display-file)
-  ("o" dired-find-file-other-window)
-  ("Q" dired-do-find-regexp-and-replace)
-  ("R" dired-do-rename)
-  ("r" dired-do-rsynch)
-  ("S" dired-do-symlink)
-  ("s" dired-sort-toggle-or-edit)
-  ("t" dired-toggle-marks)
-  ("U" dired-unmark-all-marks)
-  ("u" dired-unmark)
-  ("v" dired-view-file)      ;; q to exit, s to search, = gets line #
-  ("w" dired-kill-subdir)
-  ("Y" dired-do-relsymlink)
-  ("z" diredp-compress-this-file)
-  ("Z" dired-do-compress)
-  ("q" nil)
-  ("." nil :color blue))
-
-                                        ;https://github.com/abo-abo/hydra/wiki/PDF-Tools
+;; https://github.com/abo-abo/hydra/wiki/PDF-Tools
 (defhydra hydra-pdftools (:color blue :hint nil)
   "
                                                                       ╭───────────┐
@@ -261,11 +162,10 @@ T - tag prefix
     :states '(normal visual insert emacs)
     :prefix "SPC"
     :non-normal-prefix "M-SPC"
-    "b" '(hydra-buffer/body t :which-key "Buffer")
-    "w" '(hydra-window-operations/body t :which-key "Windows")
+    "a" '(hydra-apropos/body t :which-key "Apropos Commands")
     "f" '(hydra-flycheck/body t :which-key "Flycheck")
-    "s" '(hydra-spellchecking/body t :which-key "Spell Checking")
-    "e" '(hydra-ediff/body t :which-key "Diffing")
     "i" '(hydra-yasnippet/body t :which-key "Yasnippets")
+    "m" '(hydra-info/body t :which-key "Info")
     "p" '(hydra-pdftools/body t :which-key "PDF Tools")
-    "a" '(hydra-apropos/body t :which-key "Apropos Commands")))
+    "s" '(hydra-spellchecking/body t :which-key "Spell Checking")
+    ))

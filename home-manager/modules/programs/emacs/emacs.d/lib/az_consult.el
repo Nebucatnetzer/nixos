efc73fcab6e31001f9ad6d135026de3844303180
Andreas Zweili <andreas@zweili.ch>
2024-05-21 22:01:19 +0200

    Bind C-s to consult-line

--- home-manager/modules/programs/emacs/emacs.d/lib/az_consult.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_consult.el
@@ -27,6 +27,7 @@
   :bind
   (("C-x p" . consult-projectile)
    ("C-c k" . az-consult-git-grep-filetype)
+   ("C-s" . consult-line)
    ("C-c g". consult-projectile)))
 
 (defun az-consult-git-grep-filetype (query)

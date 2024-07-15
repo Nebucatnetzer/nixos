e109c6f77e892545ada6edc44844f1f3f4c63001
Andreas Zweili <andreas@zweili.ch>
2024-07-15 21:44:27 +0200

    Enable nix garbage collection for home-manager
    I think we never really run garbage collection for home-manager, only for NixOS itself.

--- home-manager/profiles/headless.nix
+++ home-manager/profiles/headless.nix
@@ -2,4 +2,15 @@
 {
   imports = [ "${inputs.self}/home-manager/modules" ];
   systemd.user.startServices = "sd-switch";
+  nix = {
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 30d";
+    };
+  };
+  systemd.user.timers.nix-gc = {
+    Timer = {
+      Persistent = true;
+    };
+  };
 }

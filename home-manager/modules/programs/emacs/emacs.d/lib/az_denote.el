4aae2d804b953269548fc2e528cd9ca353cc25f4
Andreas Zweili <andreas@zweili.ch>
2024-08-19 17:15:27 +0200

    Remove unused code

--- home-manager/modules/programs/emacs/emacs.d/lib/az_denote.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_denote.el
@@ -10,24 +10,6 @@
      ("C-c n t" . denote-journal-extras-new-or-existing-entry)
      ("C-c n n" . denote-subdirectory))
     :config
-    (defun az-denote-journal ()
-      "Create an entry tagged 'journal' with the date as its title. \
-       If a journal for the current day exists, visit it.\
-       If multiple entries exist, prompt with completion for a choice between them.
-       Else create a new file."
-      (interactive)
-      (let* ((today (format-time-string "%Y-%m-%d"))
-             (string (denote-sluggify-title today))
-             (files (denote-directory-files-matching-regexp string)))
-        (cond
-         ((> (length files) 1)
-          (find-file (completing-read "Select file: " files nil :require-match)))
-         (files
-          (find-file (car files)))
-         (t
-          (denote
-           today
-           '("journal"))))))
     (defun az-note-from-region (beg end)
       "Create note whose contents include the text between BEG and END. Prompt
     for title and keywords of the new note."

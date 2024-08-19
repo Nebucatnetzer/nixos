05e686e7711650342492c52eaad5e06fdbf3a180
Andreas Zweili <andreas@zweili.ch>
2024-08-19 17:23:46 +0200

    Stop ignoring the archive directory

--- home-manager/modules/programs/emacs/emacs.d/lib/az_denote.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_denote.el
@@ -26,7 +26,6 @@
     (setq denote-rename-buffer-mode 1
           denote-journal-extras-directory (concat denote-directory "99_archive/" (format-time-string "%Y") "/journal/")
           denote-journal-extras-title-format 'day-date-month-year
-          denote-excluded-directories-regexp "99_archive"
           denote-file-type "org"
           denote-directory "~/nextcloud/10_documents/"
           denote-dired-directories (list denote-directory)

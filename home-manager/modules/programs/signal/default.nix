71eb6868f24fcda03642a5e189bd21e087072025
Andreas Zweili <andreas@zweili.ch>
2024-08-26 10:16:12 +0200

    Disable autostart for Signal

--- home-manager/modules/programs/signal/default.nix
+++ home-manager/modules/programs/signal/default.nix
@@ -14,8 +14,6 @@ in
 
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [ unstable.signal-desktop ];
-
-    home.file.".config/qtile/autostart.d/signal.sh".source = ./signal.sh;
     xdg.desktopEntries = {
       signal = {
         name = "Signal with tray icon";

00ff2e2100515c9dd88f8a9602288bf84c6bcfde
Andreas Zweili <andreas@zweili.ch>
2024-09-09 23:00:05 +0200

    Refactor dap-sync
    It is now much faster and syncs cover art as well.

--- pkgs/dap-sync/dap-sync.sh
+++ pkgs/dap-sync/dap-sync.sh
@@ -1,41 +1,60 @@
+album_directories=$(mktemp)
 all_files_in_target=$(mktemp)
 playlist="$(realpath "$1")"
 source="/run/user/1000/gvfs/smb-share:server=10.7.89.108,share=media/"
 target="$2/synced"
-synced_files=$(mktemp)
+files_to_sync=$(mktemp)
 
 # stop the whole script on Ctrl-C
 trap "exit" INT
 
 sync-files-to-target() {
-    # Exlude all lines starting with #
-    # Read each line of the given playlist file.
-    # Cut the $source directory from the $file, this is required so that rsync
-    # doesn't create the full /run/user/etc. path with the --relative option.
-    # Finally sync the given file with rsync to the target and recreate the
-    # directory structure.
-
+    echo "Sync files"
     cd "$source"
-    grep -v '^#' <"$playlist" |
-        while IFS="" read -r line || [ -n "$line" ]; do
-            file=${line##$source}
-            rsync \
-                --archive \
-                --relative \
-                --info=NAME1 \
-                "$file" \
-                "$target" || break
-        done
+    rsync \
+        --archive \
+        --relative \
+        --info=NAME1 \
+        --files-from="$files_to_sync" \
+        . \
+        "$target"
 }
 
-create-synced-files-list() {
-    trap 'rm -f "$synced_files"' EXIT
-    awk '!/^#/ && NF {print "./" substr($0, length("'"$source"'") + 1)}' "$playlist" >"$synced_files"
-    sort -o "$synced_files"{,}
+create-list-of-album-directories() {
+    echo "Create list of album directories"
+    trap 'rm -f "$album_directories"' EXIT
+    while IFS="" read -r line || [ -n "$line" ]; do
+        echo "$(dirname $line)" >>"$album_directories"
+    done <"$files_to_sync"
+
+    sort -o "$album_directories"{,}
+    # Remove duplicate entries
+    awk -i inplace '!seen[$0]++' "$album_directories"
+}
+
+append-cover-art-paths() {
+    echo "Append cover art paths"
+    cd "$source"
+    while IFS="" read -r line || [ -n "$line" ]; do
+        if [ -f "$line"/cover.* ]; then
+            echo $(ls "$line"/cover.*) >>"$files_to_sync"
+        fi
+    done <"$album_directories"
+
+    sort -o "$files_to_sync"{,}
+    # remove duplicate entries
+    awk -i inplace '!seen[$0]++' "$files_to_sync"
+}
+
+convert-playlist-to-syncable-paths() {
+    echo "Convert playlist"
+    trap 'rm -f "$files_to_sync"' EXIT
+    awk '!/^#/ && NF {print "./" substr($0, length("'"$source"'") + 1)}' "$playlist" >"$files_to_sync"
 }
 
 find-all-files-in-target() {
     # Look for all files in target directory and write them to a file.
+    echo "Get all files in target"
     trap 'rm -f "$all_files_in_target"' EXIT
     cd "$target"
     find . -type f -print >"$all_files_in_target"
@@ -47,7 +66,7 @@ remove-surplus-files() {
     # Then remove the surplus files on the target.
     printf "Removing surplus files."
     cd "$target"
-    comm -23 "$all_files_in_target" "$synced_files" |
+    comm -23 "$all_files_in_target" "$files_to_sync" |
         while IFS="" read -r line || [ -n "$line" ]; do
             printf 'Removing: %s\n' "$line"
             rm "$line"
@@ -57,7 +76,9 @@ remove-surplus-files() {
 }
 
 mkdir -p "$target"
+convert-playlist-to-syncable-paths
+create-list-of-album-directories
+append-cover-art-paths
 sync-files-to-target
-create-synced-files-list
 find-all-files-in-target
 remove-surplus-files

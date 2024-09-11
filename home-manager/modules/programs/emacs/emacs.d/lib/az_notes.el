42d6b6dabece309b830e7170d9f28efb19eb2d3a
Andreas Zweili <andreas@zweili.ch>
2024-09-11 19:24:58 +0200

    Add function to get the current buffer as a command

--- home-manager/modules/programs/emacs/emacs.d/lib/az_notes.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_notes.el
@@ -5,5 +5,24 @@
     :bind ("<f5>" . open-notes))
 
   ;; A function to create a persp for my notes
-  (defun open-notes () "Create a notes perspective and open dired in the notes
-directory." (interactive) (persp-switch "notes") (dired denote-directory)))
+  (defun open-notes ()
+    "Create a notes perspective and open dired in the notes directory."
+    (interactive)
+    (persp-switch "notes")
+    (dired denote-directory))
+
+
+  (defun az-buffer-file-path-as-url ()
+    "Return the current buffer's file path as a command and copy it to the clipboard."
+    (interactive)
+    (let ((file-path (buffer-file-name)))
+      (if file-path
+          (let ((url (concat "`emacsclient " "\"" file-path "\"`")))
+            ;; Copy the URL to the clipboard
+            (kill-new url)
+            ;; Display the URL in the minibuffer
+            (message "Buffer file URL: %s (Copied to clipboard)" url)
+            ;; Return the URL
+            url)
+        (message "Current buffer is not visiting a file")
+        nil))))

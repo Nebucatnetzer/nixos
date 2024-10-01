0eccef27b13e7cdc7a5e685aac0f2b0912029c40
Andreas Zweili <andreas@zweili.ch>
2024-10-01 23:02:11 +0200

    Use better keybinding for god mode

--- home-manager/modules/programs/emacs/emacs.d/lib/az_god_mode.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_god_mode.el
@@ -7,5 +7,5 @@
 (use-package evil-god-state
   :commands evil-execute-in-god-state
   :init
-  (evil-define-key 'normal global-map "," 'evil-execute-in-god-state)
+  (evil-define-key 'normal global-map (kbd "SPC") 'evil-execute-in-god-state)
   (evil-define-key 'god global-map [escape] 'evil-god-state-bail))

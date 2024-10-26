abce15394c13dc46aacea1f3113e70e3a034ee35
Andreas Zweili <andreas@zweili.ch>
2024-10-26 12:20:31 +0200

    Use lsp-deferred with haskell

--- home-manager/modules/programs/emacs/emacs.d/lib/az_haskell.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_haskell.el
@@ -1,5 +1,5 @@
 ;; -*- lexical-binding: t; -*-
 (use-package haskell-mode
   :init
-  (add-hook 'haskell-mode-hook #'lsp)
-  (add-hook 'haskell-literate-mode-hook #'lsp))
+  (add-hook 'haskell-mode-hook #'lsp-deferred)
+  (add-hook 'haskell-literate-mode-hook #'lsp-deferred))

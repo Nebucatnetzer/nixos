05b88f1026f97071de4a2aa819f4d509000e2e99
Andreas Zweili <zweili@contria.com>
2024-11-26 15:55:33 +0100

    Extend the fzf config to be more useful

--- home-manager/modules/programs/fzf/default.nix
+++ home-manager/modules/programs/fzf/default.nix
@@ -15,6 +15,13 @@ in
   config = lib.mkIf cfg.enable {
     programs.fzf = {
       enable = true;
+      changeDirWidgetCommand = "${pkgs.fd}/bin/fd --type d . --color=never --exclude .git --exclude .devenv --exclude .direnv --exclude lost+found";
+      defaultCommand = "{pkgs.fd}/bin/fd --type f --exclude .git --exclude .devenv --exclude .direnv --strip-cwd-prefix --exclude lost+found --exclude windows-home";
+      defaultOptions = [
+        "--border"
+        "--height 40%"
+        "--walker file"
+      ];
       enableBashIntegration = true;
       tmux.enableShellIntegration = true;
     };

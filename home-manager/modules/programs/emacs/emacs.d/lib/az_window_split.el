755ff3bbcf165b3248b994edc9525668c922965b
Andreas Zweili <zweili@contria.com>
2024-12-05 17:31:10 +0100

    Map C-x C-0 to kill-buffer-and-window
    I usually don't need the buffer anymore when I do this.

--- home-manager/modules/programs/emacs/emacs.d/lib/az_window_split.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_window_split.el
@@ -41,4 +41,4 @@
 (global-set-key (kbd "C-x C-1") #'delete-other-windows)
 (global-set-key (kbd "C-x C-2") 'az-split-window-below-and-move-cursor)
 (global-set-key (kbd "C-x C-3") 'az-split-window-right-and-move-cursor)
-(global-set-key (kbd "C-x C-0") #'delete-window)
+(global-set-key (kbd "C-x C-0") #'kill-buffer-and-window)

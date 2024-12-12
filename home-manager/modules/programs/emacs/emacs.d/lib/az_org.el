4fb3b6ab1615b641b7380b98a3518ad40b8d0911
Andreas Zweili <andreas@zweili.ch>
2024-12-12 23:08:52 +0100

    Readd agenda command

--- home-manager/modules/programs/emacs/emacs.d/lib/az_org.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_org.el
@@ -19,6 +19,7 @@
                 ("C-c C-," . org-insert-structure-template))
     :config
     ;; enable org-mode keys
+    (global-set-key "\C-ca" 'org-agenda)
     (global-set-key "\C-cl" 'org-store-link)
     (global-set-key "\C-cc" 'org-capture)
     (global-set-key "\C-cb" 'org-iswitchb)

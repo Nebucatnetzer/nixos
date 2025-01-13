74850dc5c180869e79332f0a638c32935478c0d8
Andreas Zweili <andreas@zweili.ch>
2025-01-13 15:45:45 +0100

    Use latest kernel for capricorn

--- systems/capricorn/default.nix
+++ systems/capricorn/default.nix
@@ -19,7 +19,7 @@
     "usb_storage"
     "xhci_pci"
   ];
-
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [
     "kvm-intel"

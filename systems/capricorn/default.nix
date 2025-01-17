09bd87278cf20d5a762164fc94e46d10970e7c3c
Andreas Zweili <andreas@zweili.ch>
2025-01-17 18:17:57 +0100

    Add all media drivers

--- systems/capricorn/default.nix
+++ systems/capricorn/default.nix
@@ -110,6 +110,8 @@ in
     graphics = {
       enable = true;
       extraPackages = [
+        pkgs.intel-media-driver # For Broadwell (2014) or newer processors. LIBVA_DRIVER_NAME=iHD
+        pkgs.intel-vaapi-driver # For older processors. LIBVA_DRIVER_NAME=i965
         pkgs.vpl-gpu-rt
       ];
     };

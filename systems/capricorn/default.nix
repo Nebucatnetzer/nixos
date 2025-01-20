042b7d384aa3539953ca27509f062b2cb012ad0b
Andreas Zweili <andreas@zweili.ch>
2025-01-20 13:46:05 +0100

    Disable lidSwitch
    Since suspend is broken ATM I would rather not do anything when closing the lid.

--- systems/capricorn/default.nix
+++ systems/capricorn/default.nix
@@ -143,7 +143,7 @@ in
     az-zram-swap.enable = true;
     fstrim.enable = true; # Enable TRIM for SD cards
     hardware.bolt.enable = true; # Enable Thunderbolt control
-    logind.lidSwitchExternalPower = "ignore";
+    logind.lidSwitch = "ignore";
     thermald.enable = true;
   };
 }

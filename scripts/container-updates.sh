349d6f798ed690f8c8cd674a54119fbac7f47d26
Andreas Zweili <andreas@zweili.ch>
2025-02-10 16:10:22 +0100

    Sort comments

--- scripts/container-updates.sh
+++ scripts/container-updates.sh
@@ -4,11 +4,11 @@ set -e
 # Get the digest hash for the "latest" image of each image in the list.
 # In addition show the latest three tags for each image.
 
-# ../modules/services/grav/default.nix
+# ../modules/services/docker-mailserver/default.nix
 # ../modules/services/firefly/default.nix
 # ../modules/services/gitea/default.nix
+# ../modules/services/grav/default.nix
 # ../modules/services/heimdall/default.nix
-# ../modules/services/docker-mailserver/default.nix
 # ../modules/services/nextcloud/default.nix
 # ../modules/services/plex/default.nix
 images=("docker.io/linuxserver/grav" "lscr.io/linuxserver/plex" "lscr.io/linuxserver/heimdall" "docker.io/fireflyiii/data-importer" "docker.io/mailserver/docker-mailserver" "docker.io/gitea/gitea" "ghcr.io/nebucatnetzer/nextcloud-smb/nextcloud-smb")

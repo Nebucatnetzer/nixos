a65c6362f6a26288c2b59328d7f14a9c0bd407f2
Andreas Zweili <andreas@zweili.ch>
2025-02-24 20:22:14 +0100

    Change Syslog Server to gwyn

--- modules/services/syslog/default.nix
+++ modules/services/syslog/default.nix
@@ -11,7 +11,7 @@ in
     services.rsyslogd = {
       enable = true;
       defaultConfig = ''
-        *.*  action(type="omfwd" target="10.7.89.108" port="514" protocol="udp"
+        *.*  action(type="omfwd" target="10.7.89.153" port="514" protocol="udp"
                     action.resumeRetryCount="100"
                     queue.type="linkedList" queue.size="10000")
       '';

22a6189174a15a0bcd0af96bf31ddc9aacc02ed9
Andreas Zweili <andreas@zweili.ch>
2025-02-25 20:36:42 +0100

    Add syslog-ng to librenms module

--- modules/services/librenms/default.nix
+++ modules/services/librenms/default.nix
@@ -40,6 +40,25 @@ in
           ];
         };
       };
+      syslog-ng = {
+        enable = true;
+        extraConfig = ''
+          source s_net {
+            tcp(port(514) flags(syslog-protocol));
+            udp(port(514) flags(syslog-protocol));
+          };
+
+          destination d_librenms {
+            program("${pkgs.librenms}/share/syslog.php" template ("$HOST||$FACILITY||$PRIORITY||$LEVEL||$TAG||$R_YEAR-$R_MONTH-$R_DAY $R_HOUR:$R_MIN:$R_SEC||$MSG||$PROGRAM\n") template-escape(yes));
+          };
+
+          log {
+            source(s_net);
+            source(s_src);
+            destination(d_librenms);
+          };
+        '';
+      };
     };
   };
 }

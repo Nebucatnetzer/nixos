95f2b81d4777103c879f77791da26afe10cdb70f
Andreas Zweili <andreas@zweili.ch>
2025-04-28 12:19:24 +0200

    Update redis

--- modules/services/nextcloud/default.nix
+++ modules/services/nextcloud/default.nix
@@ -130,7 +130,7 @@ in
         ];
       };
       containers."redis" = {
-        image = "docker.io/redis:7.4.2-alpine@sha256:02419de7eddf55aa5bcf49efb74e88fa8d931b4d77c07eff8a6b2144472b6952";
+        image = "docker.io/redis:7.4.3-alpine";
         autoStart = true;
         volumes = [ "/etc/localtime:/etc/localtime:ro" ];
         extraOptions = [

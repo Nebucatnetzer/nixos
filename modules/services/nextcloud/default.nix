a61eb1fa2e4bd04d66c29e2f710e6b82240e86ed
Andreas Zweili <andreas@zweili.ch>
2025-07-15 19:09:51 +0200

    Update Nextcloud

--- modules/services/nextcloud/default.nix
+++ modules/services/nextcloud/default.nix
@@ -20,7 +20,7 @@ let
   };
   networkName = "nextcloud";
   # https://github.com/Nebucatnetzer/nextcloud-smb
-  nextcloudImage = "ghcr.io/nebucatnetzer/nextcloud-smb/nextcloud-smb:31.0.6@sha256:cc6e63750806584a2ad910451da9346d8bc91ce6965aa708d1fc9eb3726770a2";
+  nextcloudImage = "ghcr.io/nebucatnetzer/nextcloud-smb/nextcloud-smb:31.0.7@sha256:dbf2a25e4f1b1193558ebb1730f80794894ff4bd04cecb496782736b1a117eb4";
   nextcloudService = "${config.virtualisation.oci-containers.backend}-nextcloud";
   volumePath = "/mnt/server-data/nextcloud";
 in

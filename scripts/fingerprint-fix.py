dd34d7b9c17f5e6f0efeba935aa4747619fb1e6a
Andreas Zweili <andreas@zweili.ch>
2025-07-19 00:40:31 +0200

    Sort imports with isort

--- scripts/fingerprint-fix.py
+++ scripts/fingerprint-fix.py
@@ -4,6 +4,7 @@
 #! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/f0946fa5f1fb876a9dc2e1850d9d3a4e3f914092.tar.gz
 
 import gi
+
 gi.require_version('FPrint', '2.0')
 from gi.repository import FPrint
 

2dde60b8a7be70447ad21f009a47399e398952af
Andreas Zweili <zweili@contria.com>
2025-07-18 09:18:16 +0200

    Update ellama config

--- home-manager/modules/programs/emacs/emacs.d/lib/az_ellama.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_ellama.el
@@ -2,13 +2,12 @@
 ;; https://github.com/s-kostyaev/ellama
 
 (use-package ellama
-  :config
-  (customize-set-variable 'ellama-auto-scroll t)
+  :bind ("C-c e" . ellama)
   :init
+  (customize-set-variable 'ellama-auto-scroll t)
   ;; setup key bindings
   (setq llm-warn-on-nonfree nil)
-  ;; (setopt ellama-keymap-prefix "C-c e")
-  (global-set-key (kbd "C-c e") 'ellama-transient-main-menu)
+
   ;; language you want ellama to translate to
   (setopt ellama-language "German")
 
@@ -17,7 +16,12 @@
 
   (require 'llm-openai)
   (setopt ellama-provider (make-llm-openai
-                           :chat-model "chatgpt-4o-latest"
-                           :embedding-model "chatgpt-4o-latest"
+                           :chat-model "gpt-4.1"
+                           :embedding-model "gpt-4.1"
                            :key openai-key))
+  :config
+  ;; show ellama context in header line in all buffers
+  (ellama-context-header-line-global-mode +1)
+  ;; show ellama session id in header line in all buffers
+  (ellama-session-header-line-global-mode +1)
   )

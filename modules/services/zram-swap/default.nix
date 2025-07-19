bf96e74cdbb81c965005f381fafafe0d63cce235
Andreas Zweili <andreas@zweili.ch>
2025-07-19 11:03:43 +0200

    Remove swappiness adjustment
    I had a case where the PC nearly run to a crawl, it's probably still better to
    only swap if we really have to.
    Especially on systems with enough RAM.

--- modules/services/zram-swap/default.nix
+++ modules/services/zram-swap/default.nix
@@ -12,9 +12,5 @@ in
       enable = true;
       priority = 100;
     };
-    # Since we have "fast" swap, we can increase swappiness
-    boot.kernel.sysctl = {
-      "vm.swappiness" = 180;
-    };
   };
 }

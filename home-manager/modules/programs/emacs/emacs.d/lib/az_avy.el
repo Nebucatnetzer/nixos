a1dac2ad2d3b572cd4a5ded055c9d659b21cce7b
Andreas Zweili <andreas@zweili.ch>
2025-08-25 20:24:07 +0200

    Extend avy

--- home-manager/modules/programs/emacs/emacs.d/lib/az_avy.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_avy.el
@@ -1,5 +1,60 @@
 ;; -*- lexical-binding: t; -*-
 ;; https://github.com/abo-abo/avy
+;; https://karthinks.com/software/avy-can-do-anything
+
+(defun avy-action-copy-whole-line (pt)
+  (save-excursion
+    (goto-char pt)
+    (cl-destructuring-bind (start . end)
+        (bounds-of-thing-at-point 'line)
+      (copy-region-as-kill start end)))
+  (select-window
+   (cdr
+    (ring-ref avy-ring 0)))
+  t)
+
+(defun avy-action-embark (pt)
+  (unwind-protect
+      (save-excursion
+        (goto-char pt)
+        (embark-act))
+    (select-window
+     (cdr (ring-ref avy-ring 0))))
+  t)
+
+(defun avy-action-kill-whole-line (pt)
+  (save-excursion
+    (goto-char pt)
+    (kill-whole-line))
+  (select-window
+   (cdr
+    (ring-ref avy-ring 0)))
+  t)
+
+(defun avy-action-mark-to-char (pt)
+  (activate-mark)
+  (goto-char pt))
+
+(defun avy-action-yank-whole-line (pt)
+  (avy-action-copy-whole-line pt)
+  (save-excursion (yank))
+  t)
+
 (use-package avy
   :config
-  (global-set-key (kbd "C-;") 'avy-goto-char))
+  (setq avy-keys '(?a ?s ?d ?f ?g ?j ?l ?o
+                      ?v ?b ?n ?, ?/ ?u ?p ?e
+                      ?c ?q
+                      ))
+  (setf (alist-get ?. avy-dispatch-alist) 'avy-action-embark)
+  (setf (alist-get ?  avy-dispatch-alist) 'avy-action-mark-to-char)
+
+  (setf (alist-get ?y avy-dispatch-alist) 'avy-action-yank
+        (alist-get ?w avy-dispatch-alist) 'avy-action-copy
+        (alist-get ?W avy-dispatch-alist) 'avy-action-copy-whole-line
+        (alist-get ?Y avy-dispatch-alist) 'avy-action-yank-whole-line)
+
+  (setf (alist-get ?k avy-dispatch-alist) 'avy-action-kill-stay
+        (alist-get ?K avy-dispatch-alist) 'avy-action-kill-whole-line)
+
+  (global-set-key (kbd "C-;") 'avy-goto-char-timer))

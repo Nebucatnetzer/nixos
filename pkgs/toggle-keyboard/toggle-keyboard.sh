7a8eb32cd230dfe98c2a5ec012e8d7e1e1b0031a
Andreas Zweili <andreas@zweili.ch>
2025-08-31 13:18:35 +0200

    Fix the toggle-keyboard command
    Kanata grabs the keyboard as well. However when we use the external keyboard we
    don't need Kanata so we can safely disable it while he external keyboard is
    connected.

--- pkgs/toggle-keyboard/toggle-keyboard.sh
+++ pkgs/toggle-keyboard/toggle-keyboard.sh
@@ -2,9 +2,11 @@
 
 toggle_keyboard() {
     if pgrep "evtest"; then
+        sudo systemctl start kanata-notebook.service
         sudo pkill evtest
         printf "Internal keyboard successfully attached.\n"
     else
+        sudo systemctl stop kanata-notebook.service
         sudo evtest --grab /dev/input/event1 >/dev/null &
         printf "Internal keyboard successfully detached.\n"
     fi

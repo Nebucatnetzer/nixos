72867846b652fdb70d1ac7ad9d0fc2d8145e10bf
Andreas Zweili <andreas@zweili.ch>
2025-09-03 17:56:33 +0200

    Fix keybindings for python-pytest mode

--- home-manager/modules/programs/emacs/emacs.d/lib/az_python.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_python.el
@@ -1,15 +1,15 @@
 ;; -*- lexical-binding: t; -*-
 (use-package python-mode
   :config
-  (unbind-key "C-c C-t" python-ts-mode-map)
   (setq python-shell-interpreter "python3")
   :hook (python-ts-mode . eglot-ensure))
 
 
 (use-package python-pytest
   :config
-  :bind
-  (:map python-mode-map ("C-c C-t" . python-pytest-function-dwim)))
+  (define-key python-ts-mode-map (kbd "C-c t" ) #'python-pytest-dispatch)
+  )
+
 
 (use-package flymake-ruff
   :ensure t

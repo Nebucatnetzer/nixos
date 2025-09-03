b701b4f57ccb5a4312f352dab3381209307e98ef
Andreas Zweili <andreas@zweili.ch>
2025-09-03 15:17:37 +0200

    Extend actualbudget setup
    - backups
    - certificate watcher

--- modules/services/actualbudget/default.nix
+++ modules/services/actualbudget/default.nix
@@ -1,21 +1,35 @@
 {
   config,
+  inputs,
   lib,
+  pkgs,
   ...
 }:
 let
   cfg = config.services.az-actualbudget;
+  dataDirectory = "/var/lib/actualbudget";
+  domain = "actual.zweili.org";
 in
 {
   options = {
     services.az-actualbudget.enable = lib.mkEnableOption "Enable Actualbudget";
   };
   config = lib.mkIf cfg.enable {
+
+    # Monitor certificates
+    services.az-librenms-certificate = {
+      enable = true;
+      domains = [
+        { fqdn = "${domain}"; }
+      ];
+    };
+
+    # Webserver setup
     services.nginx = {
       recommendedOptimisation = true;
       recommendedTlsSettings = true;
       recommendedProxySettings = true;
-      virtualHosts."actual.zweili.org" = {
+      virtualHosts."${domain}" = {
         enableACME = true;
         forceSSL = true;
         locations."/" = {
@@ -33,10 +47,46 @@ in
         ports = [ "5006:5006" ];
         volumes = [
           "/etc/localtime:/etc/localtime:ro"
-          "/var/lib/actualbudget:/data"
+          "${dataDirectory}:/data"
         ];
         extraOptions = [ "--log-opt=tag='actualbudget'" ];
       };
+    };
+
+    # Backups
+    services.az-telegram-notifications.enable = true;
+    age.secrets.resticKey.file = "${inputs.self}/scrts/restic.key.age";
+    systemd.timers."restic-backups-actual" = {
+      wantedBy = [ "timers.target" ];
+      partOf = [ "restic-backups-actual.service" ];
+      timerConfig = {
+        OnCalendar = "22:40";
+      };
+    };
+
+    systemd.services."restic-backups-actual" = {
+      serviceConfig = {
+        User = "root";
+        Type = "oneshot";
+      };
+      environment = {
+        RESTIC_PASSWORD_FILE = config.age.secrets.resticKey.path;
+        RESTIC_REPOSITORY = "rest:http://10.7.89.30:8000";
+      };
+      onFailure = [ "unit-status-telegram@%n.service" ];
+      script = ''
+        ${pkgs.restic}/bin/restic backup \
+          --exclude-file=${inputs.self}/modules/misc/restic-client/excludes.txt \
+          --tag "actualbudget" ${dataDirectory}
+
+        ${pkgs.restic}/bin/restic forget \
+          --tag "actualbudget" \
+          --host ${config.networking.hostName} \
+          --keep-daily 7 \
+          --keep-weekly 5 \
+          --keep-monthly 12 \
+          --keep-yearly 2
+      '';
     };
   };
 }

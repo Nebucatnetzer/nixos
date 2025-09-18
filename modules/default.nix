dfc80893ce82f9a650762c9efe0d0366da11e2aa
Andreas Zweili <andreas@zweili.ch>
2025-09-18 19:22:06 +0200

    Remove tiling-wm-base

--- modules/default.nix
+++ modules/default.nix
@@ -61,7 +61,6 @@
     ./services/snmpd
     ./services/syslog
     ./services/telegram-notifications
-    ./services/tiling-wm-base
     ./services/tlp
     ./services/virtualbox-guest
     ./services/zram-swap

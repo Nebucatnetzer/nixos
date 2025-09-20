84f172eaa0acd380f651f8a85ccd2aa81148a39f
Andreas Zweili <andreas@zweili.ch>
2025-09-20 17:41:07 +0200

    Add todos and notes to the repo

---
+++ home-manager/modules/programs/emacs/20230723T231302--configure-emacs__emacs_project/inbox/custom.el
@@ -0,0 +1,22 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
+ '(custom-safe-themes
+   '("4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" default))
+ '(lsp-pylsp-plugins-flake8-ignore ["D100" "D103"])
+ '(lsp-pylsp-plugins-flake8-select ["D1"])
+ '(lsp-pylsp-plugins-pydocstyle-enabled nil)
+ '(package-selected-packages
+   '(counsel-tramp org-man gnu-elpa-keyring-update yasnippet-snippets yaml-mode which-key web-mode use-package-ensure-system-package treemacs-evil solarized-theme smooth-scrolling rainbow-delimiters python-mode powershell pdf-tools ox-pandoc org-superstar org-ref nix-mode move-text markdown-mode magit highlight-indent-guides helpful general flycheck eyebrowse evil-surround elpy discover-my-major direnv dired-hide-dotfiles deft counsel company-web company-restclient company-auctex bug-hunter))
+ '(warning-suppress-types '((comp) (direnv) (direnv) (direnv))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(line-number ((t (:background "unspecified-bg" :foreground "#93a1a1" :underline nil :weight thin))))
+ '(line-number-current-line ((t (:inherit line-number :background "unspecified-bg" :foreground "color-16"))))
+ '(menu ((t (:background "unspecified-bg" :foreground "#657b83")))))

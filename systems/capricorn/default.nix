a5a179ff614882e8fd66763853239c4c86312af4
Andreas Zweili <andreas@zweili.ch>
2025-10-27 13:32:28 +0100

    Use latest kernel for capricorn

--- systems/capricorn/default.nix
+++ systems/capricorn/default.nix
@@ -45,7 +45,7 @@ in
     "usb_storage"
     "xhci_pci"
   ];
-  boot.kernelPackages = pkgs.linuxPackages_6_16;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.initrd.kernelModules = [
     "xe" # graphics driver
     "dm-snapshot"

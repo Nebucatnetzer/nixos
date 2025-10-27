4047aeab0c5329ab5401f1a919c25824e44d646a
Andreas Zweili <andreas@zweili.ch>
2025-10-27 15:24:01 +0100

    Get yt-dlp from unstable
    It otherwise often doesn't work.

--- home-manager/modules/programs/yt-dlp/default.nix
+++ home-manager/modules/programs/yt-dlp/default.nix
@@ -1,6 +1,13 @@
-{ config, lib, ... }:
+{
+  config,
+  inputs,
+  lib,
+  pkgs,
+  ...
+}:
 let
   cfg = config.programs.az-yt-dlp;
+  unstable = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system};
 in
 {
   options = {
@@ -10,6 +17,7 @@ in
   config = lib.mkIf cfg.enable {
     programs.yt-dlp = {
       enable = true;
+      package = unstable.yt-dlp;
       settings = {
         remux-video = "mkv";
       };

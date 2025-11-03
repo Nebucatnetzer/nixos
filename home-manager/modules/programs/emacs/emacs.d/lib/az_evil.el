1ddbacf843929ee52bb649a852d744fb0fc0276c
Andreas Zweili <andreas@zweili.ch>
2025-11-03 21:52:47 +0100

    Remove custom function

--- home-manager/modules/programs/emacs/emacs.d/lib/az_evil.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_evil.el
@@ -46,17 +46,6 @@
 
   (evil-mode 1))
 
-;;; esc quits
-(defun minibuffer-keyboard-quit ()
-  "Abort recursive edit.
-    In Delete Selection mode, if the mark is active, just deactivate it;
-    then it takes a second \\[keyboard-quit] to abort the minibuffer."
-  (interactive)
-  (if (and delete-selection-mode transient-mark-mode mark-active)
-      (setopt deactivate-mark  t)
-    (when (get-buffer "*Completions*")
-      (delete-windows-on "*Completions*"))
-    (abort-recursive-edit)))
 (define-key evil-normal-state-map [escape] 'keyboard-quit)
 (define-key evil-visual-state-map [escape] 'keyboard-quit)
 (define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)

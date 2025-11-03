06b4392e0f04e5777667895dd115c6289ce72ce7
Andreas Zweili <andreas@zweili.ch>
2025-11-03 15:07:11 +0100

    Remove evil keybindings from perspective

--- home-manager/modules/programs/emacs/emacs.d/lib/az_perspective.el
+++ home-manager/modules/programs/emacs/emacs.d/lib/az_perspective.el
@@ -10,9 +10,5 @@
   (consult-customize consult--source-buffer :hidden t :default nil)
   (add-to-list 'consult-buffer-sources persp-consult-source)
   :init
-  (define-key evil-normal-state-map "gt" 'persp-next)
-  (define-key evil-normal-state-map "gT" 'persp-prev)
-  (define-key evil-normal-state-map "gr" 'persp-rename)
-  (define-key evil-normal-state-map "gc" 'persp-kill)
   (setopt persp-state-default-file "~/.emacs.d/persp-session")
   (persp-mode))

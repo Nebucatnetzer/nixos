04d335e30fd8678bef01571024789768586d06e8
Andreas Zweili <andreas@zweili.ch>
2025-11-13 18:14:56 +0100

    Add defaults explicitly

--- systems/capricorn/default.nix
+++ systems/capricorn/default.nix
@@ -8,7 +8,9 @@ let
   commonBtrfsOptions = [
     "compress=lzo"
     "defaults"
+    "discard=async" # be explicit about defaults
     "noatime"
+    "space_cache=v2" # be explicit about defaults
     "ssd"
   ];
   toggle-keyboard = pkgs.callPackage "${inputs.self}/pkgs/toggle-keyboard" { };

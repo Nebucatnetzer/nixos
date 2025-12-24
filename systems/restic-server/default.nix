22cd7f9fda9b382a0f5640326e4d6fa5bdf260e5
Andreas Zweili <andreas@zweili.ch>
2025-12-24 14:35:04 +0100

    Move the restic repository to an SSD
    This way we don't store the backups on the same device as the data.

--- systems/restic-server/default.nix
+++ systems/restic-server/default.nix
@@ -1,6 +1,8 @@
 { hostname }:
 { inputs, ... }:
 let
+  btrfsModule = import "${inputs.self}/modules/hardware/btrfs";
+  commonBtrfsOptions = import "${inputs.self}/modules/hardware/btrfs/common_options.nix";
   raspiEthernet = import "${inputs.self}/modules/hardware/raspi4/raspi-ethernet.nix" {
     inherit hostname;
     ip = "10.7.89.30";
@@ -9,7 +11,19 @@ in
 {
   imports = [
     "${inputs.self}/modules/profiles/server"
+    (btrfsModule { btrfsLabel = "resticSSD"; })
     raspiEthernet
   ];
+
+  fileSystems."/var/lib/restic-server" = {
+    fsType = "btrfs";
+    label = "resticSSD";
+    neededForBoot = false;
+    options = [
+      "subvol=restic-repo"
+    ]
+    ++ commonBtrfsOptions;
+  };
+
   services.az-restic-server.enable = true;
 }

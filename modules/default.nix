499a2ad90115751425c33bd187054c0395779b9d
Andreas Zweili <andreas@zweili.ch>
2025-12-27 16:18:05 +0100

    Move unused modules out of the global imports

--- modules/default.nix
+++ modules/default.nix
@@ -5,11 +5,8 @@
     inputs.home-manager.nixosModules.home-manager
     ./misc/common
     ./misc/username
-    ./services/az-librenms-agent
-    ./services/az-nginx-fpm
     ./services/az-restic-client-server
     ./services/az-restic-client-server-mysql
-    ./services/az-restic-client-server-postgres
     ./services/az-restic-server
   ];
 }

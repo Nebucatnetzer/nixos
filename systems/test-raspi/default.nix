88c9104b12cb43d629afc81a638dbdd331d7b9da
Andreas Zweili <andreas@zweili.ch>
2026-01-04 19:48:31 +0100

    Test external USB drive with BTRFS

--- systems/test-raspi/default.nix
+++ systems/test-raspi/default.nix
@@ -1,6 +1,8 @@
 { hostname }:
 { inputs, ... }:
 let
+  btrfsAuxModule = import "${inputs.self}/modules/hardware/btrfs/aux.nix";
+  commonBtrfsOptions = import "${inputs.self}/modules/hardware/btrfs/common_options.nix";
   raspi4Configs = import "${inputs.self}/modules/hardware/raspi4";
 in
 {
@@ -8,12 +10,23 @@ in
     "${inputs.self}/modules/profiles/server"
     "${inputs.self}/modules/programs/nix-direnv"
     "${inputs.self}/modules/services/docker"
+    (btrfsAuxModule {
+      filesystemName = "external-USB";
+      beesSpec = "/mnt/test-usb";
+    })
     raspi4Configs.diskLayouts.singleSdCard
     (raspi4Configs.ethernet {
       inherit hostname;
       ip = "10.7.89.40";
     })
   ];
+
+  fileSystems."/mnt/test-usb" = {
+    fsType = "btrfs";
+    device = "/dev/disk/by-partlabel/disk-testUsbStick-btrfs";
+    neededForBoot = false;
+    options = [ ] ++ commonBtrfsOptions;
+  };
 
   nixpkgs.hostPlatform = "aarch64-linux";
 }

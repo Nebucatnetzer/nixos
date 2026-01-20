0a9c172d83ac93fef74609f1a114a15374cab948
Andreas Zweili <andreas@zweili.ch>
2026-01-20 19:30:08 +0100

    Update path to music library

--- modules/home-manager/programs/beets/default.nix
+++ modules/home-manager/programs/beets/default.nix
@@ -8,7 +8,7 @@
     enable = true;
     settings = {
       asciify_paths = true;
-      directory = "/mnt/media/synced/music";
+      directory = "/mnt/media/audio/music";
       import = {
         autotag = false;
         copy = true;

ac976c9bfd7affa558afb5f63c314b1abb4d6578
Andreas Zweili <andreas@zweili.ch>
2026-02-05 20:15:02 +0100

    Change the fzf command to fileWidgetCommand

--- modules/home-manager/programs/fzf/default.nix
+++ modules/home-manager/programs/fzf/default.nix
@@ -3,7 +3,7 @@
   programs.fzf = {
     enable = true;
     changeDirWidgetCommand = "${pkgs.fd}/bin/fd --type d . --color=never --exclude .git --exclude .devenv --exclude .direnv --exclude lost+found";
-    defaultCommand = "{pkgs.fd}/bin/fd --type f --exclude .git --exclude .devenv --exclude .direnv --strip-cwd-prefix --exclude lost+found --exclude windows-home";
+    fileWidgetCommand = "{pkgs.fd}/bin/fd --type f --exclude .git --exclude .devenv --exclude .direnv --strip-cwd-prefix --exclude lost+found --exclude windows-home";
     defaultOptions = [
       "--border"
       "--height 40%"

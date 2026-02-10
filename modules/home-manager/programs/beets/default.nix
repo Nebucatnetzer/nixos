2e48b4cfca4002374ba65799e930fd9f75dd71bf
Andreas Zweili <andreas@zweili.ch>
2026-02-10 21:38:26 +0100

    Update beet settings

--- modules/home-manager/programs/beets/default.nix
+++ modules/home-manager/programs/beets/default.nix
@@ -8,7 +8,18 @@
     enable = true;
     settings = {
       asciify_paths = true;
+      convert = {
+        copy_album_art = "yes";
+        format = "opus";
+        formats = {
+          opus = "ffmpeg -i $source -y -vn -acodec libopus -ab 256k -vbr on $dest";
+        };
+        never_convert_lossy_files = true;
+      };
       directory = "/mnt/media/audio/music/music";
+      fetchart = {
+        auto = true;
+      };
       import = {
         autotag = false;
         copy = true;
@@ -17,11 +28,12 @@
         write = false;
       };
       paths = {
-        default = ''$albumartist/''${year}_''${album}/''${track}_''${title}'';
+        default = "$albumartist/\${year}_\${album}/\${track}_\${title}";
         singleton = "$artist/Non-Album/$title";
-        comp = ''Various_Artists/$album/''${track}_''${title}'';
+        comp = "Various_Artists/$album/\${track}_\${title}";
       };
       plugins = [
+        "convert"
         "export"
         "fetchart"
         "lastgenre"

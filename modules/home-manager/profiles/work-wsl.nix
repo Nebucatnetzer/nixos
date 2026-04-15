54a76b88166354c01ad7f50b942b03b914bf9ad0
Andreas Zweili <zweili@contria.com>
2026-04-15 15:03:33 +0200

    Sort variables

--- modules/home-manager/profiles/work-wsl.nix
+++ modules/home-manager/profiles/work-wsl.nix
@@ -55,10 +55,10 @@ in
     ];
     sessionPath = [ "$HOME/.local/share/node_modules/bin" ];
     sessionVariables = {
+      DEFAULT_BROWSER = "/mnt/c/Program Files/Mozilla Firefox/firefox.exe";
       NIX_PATH = "nixpkgs=${pkgs.path}";
       NPM_CONFIG_PREFIX = "$HOME/.local/share/node_modules";
       PATH = "$PATH:$HOME/.local/bin";
-      DEFAULT_BROWSER = "/mnt/c/Program Files/Mozilla Firefox/firefox.exe";
     };
   };
 

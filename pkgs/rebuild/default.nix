084fb2b889c4285c407db0cf4bd9df385f129bc9
Andreas Zweili <andreas@zweili.ch>
2026-04-16 20:03:05 +0200

    Remove binary cache from rebuild

--- pkgs/rebuild/default.nix
+++ pkgs/rebuild/default.nix
@@ -1,5 +1,4 @@
 {
-  netcat,
   nixos-rebuild-ng,
   writeShellApplication,
 }:
@@ -9,12 +8,6 @@ writeShellApplication {
     nixos-rebuild-ng
   ];
   text = ''
-    if ${netcat}/bin/nc -vzw 2 cache.zweili.org 2222; then
-      nixos-rebuild-ng -j auto switch --sudo
-      upload-to-cache /run/current-system
-    else
-      echo "Build without private cache"
-      sudo nixos-rebuild-ng switch --option substituters "https://cache.nixos.org https://nix-community.cachix.org"
-    fi
+    nixos-rebuild-ng -j auto switch --sudo
   '';
 }

71066978180b7b636e9678f63a070773c89965dc
Andreas Zweili <andreas@zweili.ch>
2026-05-07 22:18:32 +0200

    Remove unused keybinding

--- modules/home-manager/programs/emacs/emacs.d/lib/az_org.el
+++ modules/home-manager/programs/emacs/emacs.d/lib/az_org.el
@@ -35,7 +35,6 @@
     (global-set-key "\C-ca" 'org-agenda)
     (global-set-key "\C-cl" 'org-store-link)
     (global-set-key "\C-cc" 'org-capture)
-    (global-set-key "\C-cb" 'org-iswitchb)
     (define-key hyperbole-mode-map (kbd "C-c /") nil)
 
     ;; comes from the package verb ./az_verb.el

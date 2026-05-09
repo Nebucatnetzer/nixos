20ad025156c99f7753480f196dc1931846295298
Andreas Zweili <andreas@zweili.ch>
2026-05-09 15:16:28 +0200

    Replace lib.mkIf with lib.optional

--- modules/services/syncthing/default.nix
+++ modules/services/syncthing/default.nix
@@ -14,7 +14,5 @@
     dataDir = "/home/${config.az-username}/.config/syncthing";
     guiAddress = (lib.mkIf exposeWebInterface "0.0.0.0:8384");
   };
-  networking.firewall.interfaces.wg0.allowedTCPPorts = [
-    (lib.mkIf exposeWebInterface 8384)
-  ];
+  networking.firewall.interfaces.wg0.allowedTCPPorts = lib.optional exposeWebInterface 8384;
 }

3d719b8b537da363d1ca77f8b784da035b3e509f
Andreas Zweili <zweili@contria.com>
2026-05-26 13:31:25 +0200

    Update claude wrapper

--- modules/home-manager/programs/claude/claude_wrapper.nix
+++ modules/home-manager/programs/claude/claude_wrapper.nix
@@ -14,16 +14,19 @@ writeShellApplication {
     bwrap \
       --ro-bind / / \
       --tmpfs /mnt/ \
+      --ro-bind /mnt/c/Users/"$USER"/.claude/ /mnt/c/Users/"$USER"/.claude/ \
+      --ro-bind /mnt/wsl /mnt/wsl \
       --dev /dev \
       --proc /proc \
       --tmpfs /tmp \
       --tmpfs "$HOME" \
       --tmpfs "$HOME/.cache" \
       --tmpfs "$HOME/.config" \
+      --ro-bind "$HOME"/.nix-profile "$HOME"/.nix-profile \
       --bind "$PWD" "$PWD" \
       --bind "$HOME/.claude" "$HOME/.claude" \
       --bind "$HOME/.claude.json" "$HOME/.claude.json" \
-      --bind "$HOME/.config/git" "$HOME/.config/git" \
+      --ro-bind "$HOME/.config/git" "$HOME/.config/git" \
       --unshare-pid \
       --die-with-parent \
       claude "$@"

c0ae9ffb39c2e82d4a7e232824585d10a5015e06
Andreas Zweili <andreas@zweili.ch>
2026-05-31 20:48:26 +0200

    Migrate initrd

--- modules/misc/initrd-ssh/default.nix
+++ modules/misc/initrd-ssh/default.nix
@@ -1,13 +1,15 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 {
-  boot.initrd.network = {
-    enable = true;
-    ssh = {
+  boot.initrd = {
+    systemd.users.root.shell = "/bin/systemd-tty-ask-password-agent";
+    network = {
       enable = true;
-      port = 22;
-      shell = pkgs.writeShellScript "unlock-luks" "systemctl default";
-      authorizedKeys = config.users.users.${config.az-username}.openssh.authorizedKeys.keys;
-      hostKeys = [ "/etc/secrets/initrd/ssh_host_ed25519_key" ];
+      ssh = {
+        enable = true;
+        port = 22;
+        authorizedKeys = config.users.users.${config.az-username}.openssh.authorizedKeys.keys;
+        hostKeys = [ "/etc/secrets/initrd/ssh_host_ed25519_key" ];
+      };
     };
   };
 }

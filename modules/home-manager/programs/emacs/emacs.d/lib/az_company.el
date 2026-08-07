c1a1efe321312b0bd7d74e3308f7ebf70d74973e
Andreas Zweili <zweili@contria.com>
2026-08-07 08:31:22 +0200

    Complete file names

--- modules/home-manager/programs/emacs/emacs.d/lib/az_company.el
+++ modules/home-manager/programs/emacs/emacs.d/lib/az_company.el
@@ -5,6 +5,10 @@
   (setopt company-tooltip-minimum-width 25
           company-tooltip-align-annotations t
           company-dabbrev-downcase nil
-          company-idle-delay 0)
-  (add-to-list 'company-backends 'company-files)
+          company-idle-delay 0
+          ;; company-files is grouped with the others so it merges
+          ;; candidates instead of being shadowed by a backend (e.g.
+          ;; company-capf) that claims the prefix first.
+          company-backends '((company-files company-capf company-dabbrev-code company-keywords)
+                              company-dabbrev))
   (global-company-mode 1))

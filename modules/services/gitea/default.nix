75a72f62046b4f3a42fa503900305f732c5b8a89
Andreas Zweili <andreas@zweili.ch>
2026-08-17 18:36:51 +0200

    Update gitea

--- modules/services/gitea/default.nix
+++ modules/services/gitea/default.nix
@@ -61,7 +61,7 @@ in
     containers."gitea" = {
       # https://blog.gitea.io/
       # https://hub.docker.com/r/gitea/gitea/tags
-      image = "docker.io/gitea/gitea:1.27.1@sha256:34e3f6b75f5cbb6aebce588037fc5a53c84213e4d4b00da0a8d73e031a558e52";
+      image = "docker.io/gitea/gitea:1.27.2@sha256:d20286ca2b2e170fdf628e7231b8a31a3220ade39ff462b55041d43d1fc757dd";
       autoStart = true;
       environment = {
         PUID = "1000";
